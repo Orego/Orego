2c2437529e7ccb84dae9ac24f42dc2e18e4f3147
ychen <ychen@lclark.edu>
2012-06-29 15:50:53 -0700

    Revised the coefficient in the linear combination of UCT and RAVE.

--- Orego/src/orego/mcts/RaveCovPlayer.java
+++ Orego/src/orego/mcts/RaveCovPlayer.java
@@ -1,16 +1,45 @@
 package orego.mcts;
 
-public class RaveCovPlayer extends RavePlayer{
-	
-	private double raveBias;
+import static java.lang.Double.NEGATIVE_INFINITY;
+import static orego.core.Coordinates.PASS;
+import orego.core.Board;
 
-	public RaveCovPlayer() {
-		raveBias = 0.0009;
-	}
-	
+public class RaveCovPlayer extends RavePlayer {
+
+	// private double raveBias;
+	//
+	// public RaveCovPlayer() {
+	// raveBias = 0.0009;
+	// }
+
 	@Override
-	protected double raveCoefficient(double c, double rc) {
-		return (rc - c) / (rc - c + rc * c * raveBias);
+	protected SearchNode getPrototypeNode() {
+		return new RaveNode();
+	}
+
+	public double raveCoefficient(double w, double rw, double c, double rc) {
+		double raveBias = (rw / rc) - (w / c);
+		return (rc - c)
+				* (w / c)
+				* (1 - w / c)
+				/ ( (rc - 2 * c) * (w / c) * (1 - w / c) + c * (rw / rc) * (1 - rw / rc)
+						+ rc * c * raveBias * raveBias);
+	}
+
+	public double searchValue(SearchNode node, Board board, int move) {
+		if (node.getWins(move) == Integer.MIN_VALUE) {
+			return NEGATIVE_INFINITY;
+		}
+		if (move == PASS) {
+			return ((double) node.getWins(move)) / node.getRuns(move);
+		}
+		RaveNode raveNode = (RaveNode) node;
+		double c = raveNode.getRuns(move);
+		double w = raveNode.getWins(move);
+		double rc = raveNode.getRaveRuns(move);
+		double rw = raveNode.getRaveWins(move);
+		double coef = raveCoefficient(w, rw, c, rc);
+		return (w / c) * (1 - coef) + (rw / rc) * coef;
 	}
 
 }
