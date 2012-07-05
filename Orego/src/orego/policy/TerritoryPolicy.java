e6e3e501ab7adc0b753665ea43fad2a2216568fa
galbraith <galbraith@maclabcs12>
2012-07-05 15:00:06 -0700

    TerritoryPolicy tries to avoid playing in established territory.

---
+++ Orego/src/orego/policy/TerritoryPolicy.java
@@ -0,0 +1,98 @@
+package orego.policy;
+
+import static orego.core.Colors.*;
+import static orego.core.Coordinates.*;
+import orego.core.Board;
+import orego.mcts.SearchNode;
+import ec.util.MersenneTwisterFast;
+
+public class TerritoryPolicy extends Policy {
+
+	int[] weights;
+	
+	final static int TERRITORY_WEIGHT = 7;
+
+	public TerritoryPolicy() {
+		this(new RandomPolicy());
+	}
+
+	public TerritoryPolicy(Policy fallback) {
+		super(fallback);
+	}
+
+	@Override
+	public int selectAndPlayOneMove(MersenneTwisterFast random, Board board) {
+		return getFallback().selectAndPlayOneMove(random, board);
+	}
+
+	@Override
+	public void updatePriors(SearchNode node, Board board, int weight) {
+		weights = new int[EXTENDED_BOARD_AREA];
+		for (int p : ALL_POINTS_ON_BOARD) {
+			if (board.getColor(p) == BLACK) {
+				weights[p] = 64;
+			}
+			if (board.getColor(p) == WHITE) {
+				weights[p] = -64;
+			}
+		}
+		dilation(weights);
+		erosion(weights);
+		for (int p : ALL_POINTS_ON_BOARD) {
+			if (weights[p] != 0){
+				node.addLosses(p, weight * TERRITORY_WEIGHT);
+			}
+		}
+	}
+
+	public void dilation(int[] weights) {
+		int[] newweights = new int[weights.length];
+		for (int i = 0; i < newweights.length; i++) {
+			newweights[i] = weights[i];
+		}
+		for (int repeat = 0; repeat < 3; repeat++) {
+			for (int p : ALL_POINTS_ON_BOARD) {
+				for (int i = 0; i < 4; i++) {
+					int n = NEIGHBORS[p][i];
+					if (weights[n] > 0) {
+						newweights[p]++;
+					}
+					if (weights[n] < 0) {
+						newweights[p]--;
+					}
+				}
+			}
+			for (int i = 0; i < newweights.length; i++) {
+				weights[i] = newweights[i];
+			}
+		}
+	}
+	
+	public void erosion(int[] weights) {
+		int[] newweights = new int[weights.length];
+		for (int i = 0; i < newweights.length; i++) {
+			newweights[i] = weights[i];
+		}
+		for (int repeat = 0; repeat < 7; repeat++) {
+			for (int p : ALL_POINTS_ON_BOARD) {
+				for (int i = 0; i < 4; i++) {
+					int n = NEIGHBORS[p][i];
+					if (weights[p] < 0 && newweights[p] < 0) {
+						if (weights[n] >= 0) {
+							newweights[p]++;
+						}
+					}
+					else if (weights[p] > 0 && newweights[p] > 0) {
+						if (weights[n] <= 0) {
+							newweights[p]--;
+						}
+					}
+				}
+			}
+			for (int i = 0; i < newweights.length; i++) {
+				weights[i] = newweights[i];
+			}
+		}
+	}
+
+}
---
+++ Orego/src/orego/policy/TerritoryPolicyTest.java
@@ -0,0 +1,99 @@
+package orego.policy;
+
+import static org.junit.Assert.*;
+
+import orego.core.Board;
+import orego.mcts.SearchNode;
+import static orego.core.Coordinates.*;
+import static orego.core.Colors.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ec.util.MersenneTwisterFast;
+
+public class TerritoryPolicyTest {
+
+	private Board board;
+
+	private TerritoryPolicy policy;
+	
+	private SearchNode node;
+
+	private MersenneTwisterFast random;
+
+	@Before
+	public void setUp() throws Exception {
+		board = new Board();
+		random = new MersenneTwisterFast();
+		policy = new TerritoryPolicy();
+		node = new SearchNode();
+	}
+	
+	@Test
+	public void testUpdatePriors() {
+		String[] problem = new String[] { 
+				"...................",// 19
+				"...................",// 18
+				"...................",// 17
+				"...................",// 16
+				"...................",// 15
+				"...................",// 14
+				"....#..#...........",// 13
+				"...................",// 12
+				"...................",// 11
+				"...................",// 10
+				"...................",// 9
+				"...................",// 8
+				"...................",// 7
+				"...................",// 6
+				"...................",// 5
+				"...................",// 4
+				"...................",// 3
+				"...................",// 2
+				"..................." // 1
+		      // ABCDEFGHJKLMNOPQRST
+		};
+		board.setUpProblem(BLACK, problem);
+		policy.updatePriors(node, board, 2);
+		assertEquals(0, node.getWins(at("f13")));
+		assertEquals(14, node.getRuns(at("f13")));
+		assertEquals(0, node.getWins(at("d13")));
+		assertEquals(0, node.getRuns(at("d13")));
+	}
+
+	@Test
+	public void testDilation() {
+		int[] ourweights = new int[EXTENDED_BOARD_AREA];
+		ourweights[at("e13")] = 64;
+		ourweights[at("h13")] = 64;
+		policy.dilation(ourweights);
+		assertEquals(72, ourweights[at("e13")]);
+		assertEquals(72, ourweights[at("h13")]);
+		assertEquals(7, ourweights[at("f13")]);
+		assertEquals(7, ourweights[at("g13")]);
+		assertEquals(1, ourweights[at("e10")]);
+		assertEquals(2, ourweights[at("e11")]);		
+		assertEquals(6, ourweights[at("e12")]);
+		assertEquals(6, ourweights[at("d13")]);
+		assertEquals(4, ourweights[at("d12")]);
+		assertEquals(2, ourweights[at("d11")]);
+		assertEquals(0, ourweights[at("d10")]);
+	}
+
+	@Test
+	public void testErosion() {
+		int[] ourweights = new int[EXTENDED_BOARD_AREA];
+		ourweights[at("e13")] = 64;
+		ourweights[at("h13")] = 64;
+		policy.dilation(ourweights);
+		policy.erosion(ourweights);
+		assertEquals(0, ourweights[at("e10")]);
+		assertEquals(0, ourweights[at("e12")]);
+		assertEquals(0, ourweights[at("e14")]);
+		assertEquals(0, ourweights[at("d13")]);
+		assertEquals(7, ourweights[at("f13")]);
+		assertEquals(68, ourweights[at("e13")]);
+	}
+
+}
