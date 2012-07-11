20101dfbfe4f35bc4f27903a3c362be6e23fc5ac
eamundson <eamundson@maclabcs1>
2012-07-11 09:59:26 -0700

    SumResponsePlayer test passes

--- Orego/src/orego/response/SumResponsePlayer.java
+++ Orego/src/orego/response/SumResponsePlayer.java
@@ -14,21 +14,6 @@ import ec.util.MersenneTwisterFast;
 
 public class SumResponsePlayer extends ResponsePlayer {
 	
-	/** Threshold for the first level table*/
-	private int one_threshold = 100;
-	
-	/** Threshold for the second level table*/
-	private int two_threshold = 100;
-	
-	private HashMap<Integer, AbstractResponseList> responses;
-	
-	/** Weight for updateResponses */
-	private int priorsWeight;
-	
-	/** Default weight for updateResponses */
-	private static final int DEFAULT_WEIGHT = 1;
-	
-	private final int HISTORY_DEPTH = 2; 
 	
 	public static void main(String[] args) {
 		ResponsePlayer p = new ResponsePlayer();
@@ -43,68 +28,37 @@ public class SumResponsePlayer extends ResponsePlayer {
 		System.out.println("Mean: " + benchMarkInfo[0] + "\nStd Deviation: " + benchMarkInfo[1]);
 	}
 	
-	public SumResponsePlayer(){
-		super();
-		responses = new HashMap<Integer, AbstractResponseList>();
-		
-		priorsWeight = DEFAULT_WEIGHT;
-		
-		int arrayLength = Coordinates.FIRST_POINT_BEYOND_BOARD;
-		
-		// black level zero table
-		responses.put(levelZeroEncodedIndex(Colors.BLACK), new RawResponseList());
-		
-		// white level zero table
-		responses.put(levelZeroEncodedIndex(Colors.WHITE), new RawResponseList());
-
-	}
-	
 	@Override
-	public int bestStoredMove() {
-		Board board = getBoard();
-		if (board.getPasses() == 1 && secondPassWouldWinGame()) {
-			return PASS;
-		}
-		int history1 = board.getMove(board.getTurn() - 1);
-		int history2 = board.getMove(board.getTurn() - 2);
-		
-		// we need a random generator
-		// threads are stopped at this point so we are blithely unaware of synchronization concerns
-		MersenneTwisterFast random = ((McRunnable)getRunnable(0)).getRandom();
-		int move = findAppropriateMove(board, history1, history2, random);
-		
-		AbstractResponseList res = responses.get(levelTwoEncodedIndex(history2, history1, board.getColorToPlay()));
-		if(res != null && res.getWinRate(move) < 0.1) {
-			return Coordinates.RESIGN;
-		}
-		return move;
-	}
-	
 	protected int findAppropriateMove(Board board, int history1, int history2, MersenneTwisterFast random) {
 		int turn = board.getTurn();
 		int colorToPlay = board.getColorToPlay();
-		int sum = 0;
+		double sum = 0;
 		int currMove;
 		int bestMove = Coordinates.PASS;
-		int bestSum = 0;
+		double bestSum = 0;
 		
 		// pick table based on threshold values
-		RawResponseList twoList = (RawResponseList)responses.get(levelTwoEncodedIndex(history2, history1, colorToPlay));
-		RawResponseList oneList = (RawResponseList)responses.get(levelOneEncodedIndex(history1, colorToPlay));
-		RawResponseList zeroList = (RawResponseList)responses.get(levelZeroEncodedIndex(colorToPlay));
+		RawResponseList twoList = (RawResponseList)getResponses().get(levelTwoEncodedIndex(history2, history1, colorToPlay));
+		RawResponseList oneList = (RawResponseList)getResponses().get(levelOneEncodedIndex(history1, colorToPlay));
+		RawResponseList zeroList = (RawResponseList)getResponses().get(levelZeroEncodedIndex(colorToPlay));
 		
 		IntSet vacantPoints = board.getVacantPoints();
 		for(int i = 0; i < vacantPoints.size(); i++) {
 			currMove = vacantPoints.get(i);
-			sum = zeroList.getWins(currMove);
-			sum += oneList.getWins(currMove);
-			sum += twoList.getWins(i);
+			int wins = zeroList.getWins(currMove);
+			wins += oneList.getWins(currMove);
+			wins += twoList.getWins(currMove);
+			int runs = zeroList.getRuns(currMove);
+			runs += oneList.getRuns(currMove);
+			runs += twoList.getRuns(currMove);
+			sum = (double)wins/runs;
+			if(sum > bestSum){
+				bestSum = sum;
+				bestMove = currMove;
+			}
 		}
 		return bestMove;
 		
 	}
-	
-	public HashMap<Integer, AbstractResponseList> getResponses() {
-		return responses;
-	}
+
 }
--- Orego/src/orego/response/SumResponsePlayerTest.java
+++ Orego/src/orego/response/SumResponsePlayerTest.java
@@ -15,48 +15,41 @@ public class SumResponsePlayerTest {
 
 	SumResponsePlayer player;
 
-	
 	@Before
 	public void setup() {
 		player = new SumResponsePlayer();
+		player.reset();
 	}
-	
-	//TODO 
+
 	@Test
 	public void testFindAppropriateMove() {
 		// play a fake game
 		McRunnable runnable = new McRunnable(player, null);
 		int move1 = Coordinates.at("a5");
 		int move2 = Coordinates.at("a6");
-		int white1 = Coordinates.at("b5");
-		//int white2 = Coordinates.at("b5");
 		int history1 = Coordinates.at("h1");
 		int history2 = Coordinates.at("h2");
-		
-		// TODO
+
 		runnable.acceptMove(history2);
-		//runnable.acceptMove(white1);
 		runnable.acceptMove(history1);
-		//runnable.acceptMove(white2);
 		runnable.acceptMove(move1);
-		player.incorporateRun(Colors.BLACK,runnable);
-		//runnable.acceptMove(white1);
-		
-		//runnable.getBoard().clear();
-		player.getBoard().clear();
-		
+		player.incorporateRun(Colors.BLACK, runnable);
+
+		runnable.getBoard().clear();
+
 		runnable.acceptMove(history2);
-		//runnable.acceptMove(white1);
 		runnable.acceptMove(history1);
-		//runnable.acceptMove(white2);
 		runnable.acceptMove(move2);
-		player.incorporateRun(Colors.BLACK,runnable);
-		
-		HashMap<Integer, AbstractResponseList> responses = player.getResponses();
-		int index2 = ResponsePlayer.levelTwoEncodedIndex(history2, history1, Colors.BLACK);
-		int index1 = ResponsePlayer.levelOneEncodedIndex(history1, Colors.BLACK);
+		player.incorporateRun(Colors.BLACK, runnable);
+
+		HashMap<Integer, AbstractResponseList> responses = player
+				.getResponses();
+		int index2 = ResponsePlayer.levelTwoEncodedIndex(history2, history1,
+				Colors.BLACK);
+		int index1 = ResponsePlayer
+				.levelOneEncodedIndex(history1, Colors.BLACK);
 		int index0 = ResponsePlayer.levelZeroEncodedIndex(Colors.BLACK);
-		for(int i = 0; i < 10; i++) {
+		for (int i = 0; i < 10; i++) {
 			assertNotNull(responses);
 			assertNotNull(responses.get(index1));
 			responses.get(index1).addWin(move1);
@@ -65,7 +58,7 @@ public class SumResponsePlayerTest {
 			responses.get(index0).addWin(move2);
 		}
 		responses.get(index2).addWin(move2);
-		assertEquals(move2, player.bestMove());
+		assertEquals(move2, player.bestStoredMove());
 	}
 
 }
