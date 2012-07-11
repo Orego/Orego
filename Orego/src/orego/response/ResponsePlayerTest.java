c45ad3148bccb09716060c1df270417f31d6c89c
sstewart <sstewart@lclark.edu>
2012-07-11 16:35:08 -0700

    [fix] calling bestMove() instead of bestStoredMove() in ResponsePlayerTest

--- Orego/src/orego/response/ResponsePlayerTest.java
+++ Orego/src/orego/response/ResponsePlayerTest.java
@@ -5,14 +5,16 @@ import static orego.core.Colors.WHITE;
 import static orego.core.Coordinates.BOARD_WIDTH;
 import static orego.core.Coordinates.PASS;
 import static orego.core.Coordinates.at;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
 
 import java.util.HashMap;
 
 import orego.core.Colors;
 import orego.mcts.McRunnable;
-import orego.play.UnknownPropertyException;
 import orego.policy.RandomPolicy;
 
 import org.junit.Before;
@@ -121,179 +123,146 @@ public class ResponsePlayerTest {
 	
 	@Test
 	public void testPassInSeki() {
-		if (BOARD_WIDTH == 19) {
-			String[] problem = { 
-					"#.#########OOOOOOOO",// 19
-					".##########OOOOOOO.",// 18
-					"###########OOOOOOOO",// 17
-					"###########OOOOOOO.",// 16
-					"###########OOOOOOOO",// 15
-					"#########OOOOOOOOOO",// 14
-					"#########OOOOOOOOOO",// 13
-					"#########OOOOOOOOOO",// 12
-					"#########OOOOOOOOOO",// 11
-					"#########OOOOOOOOOO",// 10
-					"#########OOOOOOOOOO",// 9
-					"#########OOOOOOOOOO",// 8
-					"#########OOOOOOOOOO",// 7
-					"#########OOOOOOOOOO",// 6
-					"#########OOOOOOOOOO",// 5
-					"##OO###OOOO######OO",// 4
-					"OOO#OO#########OOOO",// 3
-					".OO#.O#.######OOOOO",// 2
-					"O.O#.O##.######OOOO" // 1
-			// ABCDEFGHJKLMNOPQRST
-			};
-			player.setUpProblem(BLACK, problem);
-			McRunnable runnable = new McRunnable(player, new RandomPolicy());
-			
-			for (int i = 0; i < 1000; i++) {
-				runnable.performMcRun();
-				//System.out.println(player.getResponseZeroBlack().getTotalRuns());
-			}
-			int move = player.bestMove();
-			assertEquals(PASS, move);
-		} else {
-			String[] problem = { "#.####OOO", ".#####OO.", "######OOO",
-					"#####OOO.", "####OOOOO", "##OO###OO", "OOO#OO###",
-					".OO#.O#.#", "O.O#.O##." };
-			player.setUpProblem(BLACK, problem);
-			int move = player.bestMove();
-			assertEquals(PASS, move);
+		String[] problem = { 
+				"#.#########OOOOOOOO",// 19
+				".##########OOOOOOO.",// 18
+				"###########OOOOOOOO",// 17
+				"###########OOOOOOO.",// 16
+				"###########OOOOOOOO",// 15
+				"#########OOOOOOOOOO",// 14
+				"#########OOOOOOOOOO",// 13
+				"#########OOOOOOOOOO",// 12
+				"#########OOOOOOOOOO",// 11
+				"#########OOOOOOOOOO",// 10
+				"#########OOOOOOOOOO",// 9
+				"#########OOOOOOOOOO",// 8
+				"#########OOOOOOOOOO",// 7
+				"#########OOOOOOOOOO",// 6
+				"#########OOOOOOOOOO",// 5
+				"##OO###OOOO######OO",// 4
+				"OOO#OO#########OOOO",// 3
+				".OO#.O#.######OOOOO",// 2
+				"O.O#.O##.######OOOO" // 1
+		// ABCDEFGHJKLMNOPQRST
+		};
+		player.setUpProblem(BLACK, problem);
+		McRunnable runnable = new McRunnable(player, new RandomPolicy());
+		
+		for (int i = 0; i < 1000; i++) {
+			runnable.performMcRun();
 		}
+		int move = runnable.getPlayer().bestMove();
+		assertEquals(PASS, move);
+
 	}
 	
 	@Test
 	public void testKillDeadStonesToOvercomeLargeTerritory() {
-		if (BOARD_WIDTH == 19) {
-			String[] problem = { "#..................",// 19
-					"O..................",// 18
-					"O..................",// 17
-					"O..................",// 16
-					"O..................",// 15
-					"O..................",// 14
-					"OOOOOOOOOOOOOOOOOOO",// 13
-					"OOOOOOOOOOOOOOOOOOO",// 12
-					"OOOOOOOOOOOOOOOOOOO",// 11
-					"OOOOOOOOOOOOOOOOOOO",// 10
-					"###################",// 9
-					"#..................",// 8
-					"#..................",// 7
-					"#..................",// 6
-					"#..................",// 5
-					"#..................",// 4
-					"#..................",// 3
-					"####...............",// 2
-					".#.#..............." // 1
-			// ABCDEFGHJKLMNOPQRST
-			};
-			player.getBoard().setUpProblem(BLACK, problem);
-			McRunnable runnable = new McRunnable(player, new RandomPolicy());
-			player.acceptMove(PASS);
-			for (int i = 0; i < 1000; i++) {
-				runnable.performMcRun();
-			}
-			int move = player.bestMove();
-			// White must capture the dead black stone to win
-			assertFalse(PASS == move);
-		} else {
-			String[] problem = { "#........", "O........", "OOOOOOOOO",
-					"OOOOOOOOO", "OOOOOOOOO", "#########", "#........",
-					"####.....", ".#.#.....", };
-			player.getBoard().setUpProblem(BLACK, problem);
-			player.acceptMove(PASS);
-			int move = player.bestMove();
-			// White must capture the dead black stone to win
-			assertFalse(PASS == move);
+		String[] problem = { "#..................",// 19
+				"O..................",// 18
+				"O..................",// 17
+				"O..................",// 16
+				"O..................",// 15
+				"O..................",// 14
+				"OOOOOOOOOOOOOOOOOOO",// 13
+				"OOOOOOOOOOOOOOOOOOO",// 12
+				"OOOOOOOOOOOOOOOOOOO",// 11
+				"OOOOOOOOOOOOOOOOOOO",// 10
+				"###################",// 9
+				"#..................",// 8
+				"#..................",// 7
+				"#..................",// 6
+				"#..................",// 5
+				"#..................",// 4
+				"#..................",// 3
+				"####...............",// 2
+				".#.#..............." // 1
+		// ABCDEFGHJKLMNOPQRST
+		};
+		player.getBoard().setUpProblem(BLACK, problem);
+		McRunnable runnable = new McRunnable(player, new RandomPolicy());
+		player.acceptMove(PASS);
+		for (int i = 0; i < 1000; i++) {
+			runnable.performMcRun();
 		}
+		int move = runnable.getPlayer().bestMove();
+		// White must capture the dead black stone to win
+		assertFalse(PASS == move);
 	}
 	
 	@Test
 	public void testKillDeadStonesToOvercomeLargeTerritory2() {
-		if (BOARD_WIDTH == 19) {
-			// Orego must realize that its own stone is dead.
-			String[] problem = { "##.O.OOOOOOOOOOOOO.",// 19
-					"OOOOOOOOOOOOOOOOOOO",// 18
-					"OOOOOOOOOOOOOOOOOOO",// 17
-					"OOOOOOOOOOOOOOOOOOO",// 16
-					"OOOOOOOOOOOOOOOOOOO",// 15
-					"OOOOOOOOOOOOOOOOOOO",// 14
-					"OOOOOOOOOOOOOOOOOOO",// 13
-					"OOOOOOOOOOOOOOOOOOO",// 12
-					"OOOOOOOOOOOOOOOOOOO",// 11
-					"OOOOOO#############",// 10
-					"###################",// 9
-					"###################",// 8
-					"###################",// 7
-					"###################",// 6
-					"###################",// 5
-					"###################",// 4
-					"####.O#############",// 3
-					"#######.###########",// 2
-					".#.##..############" // 1
-			// ABCDEFGHJKLMNOPQRST
-			};
-			player.getBoard().setUpProblem(BLACK, problem);
-			McRunnable runnable = new McRunnable(player, new RandomPolicy());
-			player.acceptMove(PASS);
-			for (int i = 0; i < 10000; i++) {
-				runnable.performMcRun();
-			}
-			int move = player.bestMove();
-			// White must capture the dead black stones to win
-			// assertEquals(at("c19"), move);
-			assertFalse(PASS == move);
-			player.getBoard().play(move);
-		} else {
-			// Orego must realize that its own stone is dead
-			String[] problem = { "##.O.OOO.", "OOOOOOOOO", "OOOOOOOOO",
-					"OOOOOOOOO", "########O", "#########", "##O.#####",
-					"####.##.#", ".#.######", };
-			player.getBoard().setUpProblem(BLACK, problem);
-			player.acceptMove(PASS);
-			int move = player.bestMove();
-			// White must capture the dead black stone to win
-			assertEquals(at("c9"), move);
-			assertFalse(PASS == move);
+		// Orego must realize that its own stone is dead.
+		String[] problem = { "##.O.OOOOOOOOOOOOO.",// 19
+				"OOOOOOOOOOOOOOOOOOO",// 18
+				"OOOOOOOOOOOOOOOOOOO",// 17
+				"OOOOOOOOOOOOOOOOOOO",// 16
+				"OOOOOOOOOOOOOOOOOOO",// 15
+				"OOOOOOOOOOOOOOOOOOO",// 14
+				"OOOOOOOOOOOOOOOOOOO",// 13
+				"OOOOOOOOOOOOOOOOOOO",// 12
+				"OOOOOOOOOOOOOOOOOOO",// 11
+				"OOOOOO#############",// 10
+				"###################",// 9
+				"###################",// 8
+				"###################",// 7
+				"###################",// 6
+				"###################",// 5
+				"###################",// 4
+				"####.O#############",// 3
+				"#######.###########",// 2
+				".#.##..############" // 1
+		// ABCDEFGHJKLMNOPQRST
+		};
+		player.getBoard().setUpProblem(BLACK, problem);
+		McRunnable runnable = new McRunnable(player, new RandomPolicy());
+		player.acceptMove(PASS);
+		for (int i = 0; i < 10000; i++) {
+			runnable.performMcRun();
 		}
+		int move = runnable.getPlayer().bestMove();
+		// White must capture the dead black stones to win
+		// assertEquals(at("c19"), move);
+		assertFalse(PASS == move);
 	}
 	
 	@Test
-	public void testConnect() throws UnknownPropertyException {
-		if (BOARD_WIDTH == 19) {
-			String[] problem = { 
-					"##########OOOOOOOOO",// 19
-					"##########OOOOOOOOO",// 18
-					"##..######OOOOOOOOO",// 17
-					"###.######OOOO..OOO",// 16
-					"##########OOOO.OOOO",// 15
-					"##########OOOOOOOOO",// 14
-					"##########OOOOOOOOO",// 13
-					"##########OOOOOOOOO",// 12
-					"##########OOOOOOOOO",// 11
-					"OOOOOOOOO.OOOOOOOOO",// 10
-					"OOOOOOOOO##########",// 9
-					"OOOOOOOOO##########",// 8
-					"OOOOOOOOO##########",// 7
-					"OOOOOOOOO##########",// 6
-					"OOOO.OOOO##########",// 5
-					"OOOO..OOO#####.####",// 4
-					"OOOOOOOOO####..####",// 3
-					"OOOOOOOOO##########",// 2
-					"OOOOOOOOO##########" // 1
-			// ABCDEFGHJKLMNOPQRST
-			};
-			player.setUpProblem(BLACK, problem);
-			//TODO: check level zero and one tables -- chooses move PASS
-			//when not consulting level two table
-			
+	public void testConnect() {
+		String[] problem = { 
+				"##########OOOOOOOOO",// 19
+				"##########OOOOOOOOO",// 18
+				"##..######OOOOOOOOO",// 17
+				"###.######OOOO..OOO",// 16
+				"##########OOOO.OOOO",// 15
+				"##########OOOOOOOOO",// 14
+				"##########OOOOOOOOO",// 13
+				"##########OOOOOOOOO",// 12
+				"##########OOOOOOOOO",// 11
+				"OOOOOOOOO.OOOOOOOOO",// 10
+				"OOOOOOOOO##########",// 9
+				"OOOOOOOOO##########",// 8
+				"OOOOOOOOO##########",// 7
+				"OOOOOOOOO##########",// 6
+				"OOOO.OOOO##########",// 5
+				"OOOO..OOO#####.####",// 4
+				"OOOOOOOOO####..####",// 3
+				"OOOOOOOOO##########",// 2
+				"OOOOOOOOO##########" // 1
+			  // ABCDEFGHJKLMNOPQRST
+		};
+		player.setUpProblem(BLACK, problem);
+		//TODO: check level zero and one tables -- chooses move PASS
+		//when not consulting level two table
+		
 
-			McRunnable runnable = new McRunnable(player, new RandomPolicy());
-			for (int i = 0; i < 10000; i++) {
-				runnable.performMcRun();
-			}
-			int move = player.bestMove();
-			assertEquals(at("k10"), move);
-		} 
-	}
+		McRunnable runnable = new McRunnable(player, new RandomPolicy());
+		for (int i = 0; i < 100; i++) {
+			runnable.performMcRun();
+		}
+		
+		
+		// Note: best stored move finds the final move after all the Mc
+		int move = runnable.getPlayer().bestStoredMove();
+		assertEquals(at("k10"), move);
+	} 
 }
