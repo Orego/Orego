daa1d7f66b79e4d61ea054db1f6f6a4b71eb4ce6
drake <drake@lclark.edu>
2012-07-16 10:34:47 -0700

    Modified orego.play.Player to allow command-line specification of policies not in the orego.policy package.

--- Orego/src/orego/play/Player.java
+++ Orego/src/orego/play/Player.java
@@ -201,7 +201,7 @@ public class Player implements Playable {
 			Policy prototype = null;
 			for (int i = policyClasses.length - 1; i >= 0; i--) {
 				String genClass = policyClasses[i];
-				if (!genClass.startsWith("orego.")) {
+				if (!genClass.startsWith("orego.") && genClass.indexOf(".") < 0) {
 					// set default path to policies if it isn't given
 					genClass = "orego.policy." + genClass;
 				}
