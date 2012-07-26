cc51c1061779d62272c40cfca1d16f96097e8fea
jetaime <jetaime@lclark.edu>
2012-07-26 14:25:52 -0700

    Made some changes to Line heuristic

--- Orego/src/orego/heuristic/LineHeuristic.java
+++ Orego/src/orego/heuristic/LineHeuristic.java
@@ -6,18 +6,21 @@ import orego.core.Coordinates;
 public class LineHeuristic extends Heuristic {
 
 	private static double[] values;
+	
+	public LineHeuristic(double weight) {
+		setWeight(weight);
+	}
 
 	static {
-		values = new double[Coordinates.BOARD_AREA];
+		values = new double[Coordinates.EXTENDED_BOARD_AREA];
 		if (Coordinates.BOARD_WIDTH == 9) {
-			for (int i = 0; i < Coordinates.BOARD_AREA; i++) {
+			for (int i = 0; i < Coordinates.FIRST_POINT_BEYOND_BOARD; i++) {
 				values[i] = Math
 						.exp(-(((Coordinates.row(Coordinates.ALL_POINTS_ON_BOARD[i]) - 5)
 								* (Coordinates.row(Coordinates.ALL_POINTS_ON_BOARD[i]) - 5) + (Coordinates
 								.column(Coordinates.ALL_POINTS_ON_BOARD[i]) - 5) * (Coordinates.column(Coordinates.ALL_POINTS_ON_BOARD[i]) - 5))) / 4.5);
 			}
 		}
-
 	}
 
 	@Override
