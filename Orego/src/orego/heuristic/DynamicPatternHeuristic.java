d13ce591dde76da70440654669a8043cbdd7c25f
galbraith <galbraith@maclabcs8.lclark.edu>
2012-08-03 15:38:08 -0700

    Fixed DynamicPatternHeuristic to match new architecture.

--- Orego/src/orego/heuristic/DynamicPatternHeuristic.java
+++ Orego/src/orego/heuristic/DynamicPatternHeuristic.java
@@ -63,26 +63,17 @@ public class DynamicPatternHeuristic extends Heuristic {
 		} 
 	}
 
-	@Override
-	public void prepare(Board board) {
-		int p = board.getMove(board.getTurn() - 1);
-		long pattern1 = DynamicPattern.setupPattern(p, board, 8);
-		long pattern2 = DynamicPattern.setupPattern(p, board, 12);
-		long pattern3 = DynamicPattern.setupPattern(p, board, 20);
-		long pattern4 = DynamicPattern.setupPattern(p, board, 24);
-		for (DynamicPattern pattern : patternList) {
-			if (pattern.match(pattern4, 24)) {
-				return 4 * getWeight();
+	public int evaluate(int p, Board board) {
+		int returnValue = 0;
+		long pattern8 = DynamicPattern.setupPattern(p, board, 8);
+		long pattern12 = DynamicPattern.setupPattern(p, board, 12);
+		long pattern20 = DynamicPattern.setupPattern(p, board, 20);
+		long pattern24 = DynamicPattern.setupPattern(p, board, 24);
+		for (DynamicPattern pattern : pattern24List) {
+			if (pattern.match(pattern24, 24)) {
+				returnValue += 4 * getWeight();
 			}
-			if (pattern.match(pattern3, 20)) {
-				return 3 * getWeight();
-			}
-			if (pattern.match(pattern2, 12)) {
-				return 2 * getWeight();
-			}
-			if (pattern.match(pattern1, 8)) {
-				return 1 * getWeight();
-		}
+		}		
 		for (DynamicPattern pattern : pattern20List) {
 			if (pattern.match(pattern20, 20)) {
 				returnValue += 3 * getWeight();
@@ -101,19 +92,18 @@ public class DynamicPatternHeuristic extends Heuristic {
 		return returnValue;
 	}
 	
-	public void prepare(Board board, MersenneTwisterFast random) {
-		super.prepare(board, random);
-		int[] values = getValues();
+	public void prepare(Board board) {
+		super.prepare(board);
 		for (int p : NEIGHBORS[board.getMove(board.getTurn() - 1)]) {
 			if (board.getColor(p) == VACANT) {
-				values[p] = evaluate(p, board);
-				if (values[p] > 0) {
-					getNonzeroPoints().add(p);
+				int playValue = evaluate(p, board);
+				if(playValue > 0) {
+					recommend(p);
+				}
+				if(playValue < 0) {
+					discourage(p); 
 				}
 			}
-		}
-		if (getNonzeroPoints().size() > 0) {
-			setBestMove(getNonzeroPoints().get(random.nextInt(getNonzeroPoints().size())));
 		}
 	}
 
