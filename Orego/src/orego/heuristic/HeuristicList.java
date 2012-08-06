2cc0497827d74dae3a14e51640f9969528fab0b5
Peter Drake <drake@lclark.edu>
2012-08-06 14:12:15 -0700

    Experiment

--- Orego/src/orego/heuristic/HeuristicList.java
+++ Orego/src/orego/heuristic/HeuristicList.java
@@ -256,7 +256,6 @@ public class HeuristicList implements Cloneable {
 					return;
 				}
 			}
-			
 			// create heuristic if it doesn't exist
 			Heuristic newHeuristic = appendNewHeuristic(heuristicName + "Heuristic");
 			newHeuristic.setProperty(heuristicProperty, value);
