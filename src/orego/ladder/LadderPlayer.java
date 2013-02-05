0079cc84b7b4a378b817606842386da5d1f16801
erikbean <erikbean@maclabcs6.lclark.edu>
2013-02-04 17:11:03 -0800

    ladder reader new branch and package sam sam erik

---
+++ src/orego/ladder/LadderPlayer.java
@@ -0,0 +1,17 @@
+package orego.ladder;
+
+public class LadderPlayer {
+
+	public LadderPlayer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
