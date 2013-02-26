fa31abca367b275f14c4896d2108a5de1f117497
samuelkelly <samuel.kelly@gmail.com>
2013-02-25 18:56:35 -0800

    Modified LadderPlayer to bias search tree with incorporateRun() method.

--- src/orego/ladder/LadderPlayer.java
+++ src/orego/ladder/LadderPlayer.java
@@ -4,34 +4,23 @@ import orego.mcts.*;
 import orego.util.IntSet;
 import orego.core.*;
 
-// CHANGE (2)
-
 /**
  * This player plays out ladders and biases the point that kicks each one off,
  * either for play (if we win) or against play (if we lose).
  */
 public class LadderPlayer extends Lgrf2Player {
-
-	/** Copy of the board for playing out ladders. */
-	private Board ladBoard;
 	
 	McRunnable runnable;
 
-	public LadderPlayer() {
-		 ladBoard = new Board();
-	}
-	
 	@Override
 	/** calls playOutLadders after stopping threads to bias stored moves*/	
-	public int bestMove(){
+	public int bestMove() {
 		try {
-			System.err.println(getRoot().getTotalRuns());
-			System.err.println(getRoot().getTotalRuns());
+//			System.err.println(getRoot().getTotalRuns());
+//			System.err.println(getRoot().getTotalRuns());
 			stopThreads();
-			ladBoard = new Board();
-			ladBoard.copyDataFrom(getBoard());
 			playOutLadders();
-			runThreads();
+ 			runThreads();
 		} catch (InterruptedException shouldNotHappen) {
 			shouldNotHappen.printStackTrace();
 			System.exit(1);
@@ -45,11 +34,11 @@ public class LadderPlayer extends Lgrf2Player {
 	public IntSet libertiesOfLadders(int color) {
 		// our definition of a ladder is: a chain in atari whose
 		// liberty has exactly two vacant neighbors
-		IntSet chainsInAtari = ladBoard.getChainsInAtari(color);
+		IntSet chainsInAtari = getBoard().getChainsInAtari(color);
 		IntSet libertiesOfLadders = new IntSet (Coordinates.FIRST_POINT_BEYOND_BOARD);
 		for (int i = 0; i < chainsInAtari.size(); i++) {
-			int liberty = ladBoard.getLibertyOfChainInAtari(chainsInAtari.get(i));
-			if (ladBoard.getVacantNeighborCount(liberty) == 2) {
+			int liberty = getBoard().getLibertyOfChainInAtari(chainsInAtari.get(i));
+			if (getBoard().getVacantNeighborCount(liberty) == 2) {
 				libertiesOfLadders.add(liberty);
 			}
 		}
@@ -68,28 +57,32 @@ public class LadderPlayer extends Lgrf2Player {
 		int numberOfBlackLadders = ladderLiberties.size();
 		ladderLiberties.addAll(libertiesOfLadders(Colors.WHITE));
 		
+		Board localBoard = new Board();
+		
 		// play out each ladder separately
 		for (int i = 0; i < ladderLiberties.size(); i++) {
 			
+			int ladderLength = 0;
+			
 			// we'll be moving, so we use a local copy of the board
-			ladBoard.copyDataFrom(getBoard());
-			runnable.copyDataFrom(getBoard());
+			localBoard.copyDataFrom(getBoard());
+			runnable.copyDataFrom(localBoard);
 			
 			int liberty = ladderLiberties.get(i);
 			int insideColor = (i < numberOfBlackLadders) ? Colors.BLACK : Colors.WHITE;
 			boolean insideWon;
 			
 			// start as the inside color
-			ladBoard.setColorToPlay(insideColor);
+			localBoard.setColorToPlay(insideColor);
 			
 			// keep applying the policy of the inside player and the outside player until
 			// the ladder is over (either the inside color is free or has been captured)
 			while (true) {
 				// inside player policy: play in my only liberty
 				int insidePlaysHere = liberty;
-				ladBoard.play(insidePlaysHere);
+				localBoard.play(insidePlaysHere);
 				runnable.acceptMove(insidePlaysHere);
-				if (ladBoard.getLibertyCount(insidePlaysHere) >= 3)	{
+				if (localBoard.getLibertyCount(insidePlaysHere) >= 3) {
 					// inside player is free
 					insideWon = true;
 					break;
@@ -97,49 +90,43 @@ public class LadderPlayer extends Lgrf2Player {
 				
 				// outside player policy: play in the inside player's liberty
 				// with the most vacant neighbors
-				IntSet libsOfNewChain = ladBoard.getLiberties(insidePlaysHere);
+				IntSet libsOfNewChain = localBoard.getLiberties(insidePlaysHere);
 				int mostVacantNeighbors = -1;
 				int pointWithMostVacantNeighbors = Coordinates.FIRST_POINT_BEYOND_BOARD;
 				
 				for (int j = 0; j < libsOfNewChain.size(); j++)	{
-					if (ladBoard.getVacantNeighborCount(libsOfNewChain.get(j)) > mostVacantNeighbors) {
+					if (localBoard.getVacantNeighborCount(libsOfNewChain.get(j)) > mostVacantNeighbors) {
 						pointWithMostVacantNeighbors = libsOfNewChain.get(j);
-						mostVacantNeighbors = ladBoard.getVacantNeighborCount(pointWithMostVacantNeighbors);
+						mostVacantNeighbors = localBoard.getVacantNeighborCount(pointWithMostVacantNeighbors);
 					}
 				}
-				ladBoard.play(pointWithMostVacantNeighbors);
+				localBoard.play(pointWithMostVacantNeighbors);
 				runnable.acceptMove(pointWithMostVacantNeighbors);
-				if (ladBoard.getColor(insidePlaysHere) != insideColor) {
+				if (localBoard.getColor(insidePlaysHere) != insideColor) {
 					// inside player was captured
 					insideWon = false;
 					break;
 				}
 				
+				ladderLength++;
+
 				// add code here looking for outside stones in atari
-				
 			}
 			
-			// bias the search tree appropriately:
-			// - if inside player wins, he wants to play here and
-			//   *so does outside player* (to cut off the ladder)
-			// - if inside player loses, he doesn't want to play here and 
-			//   *neither does outside player* (to allow inside player to do so)
+			// bias the search tree: call this a playout for the winner
 			if (insideWon) {
-				for (int j = 0; j < 500; j++) {
+				for (int j = 0; j < ladderLength*10; j++) {
 					incorporateRun(insideColor, runnable);
 				}
-				//getRoot().addWins(liberty, 10);
 				System.err.println("Biasing in favor of " + Coordinates.pointToString(liberty));
 			} else {
-				for (int j = 0; j < 500; j++) {
+				for (int j = 0; j < ladderLength*10; j++) {
 					incorporateRun(Colors.opposite(insideColor), runnable);
 				}
-				//getRoot().addLosses(liberty, 10);
 				System.err.println("Biasing against " + Coordinates.pointToString(liberty));
-
 			}
 			
-			System.err.println(ladBoard);
+			System.err.println(localBoard);
 		}
 	}		
 }
--- src/orego/ladder/LadderPlayerTest.java
+++ src/orego/ladder/LadderPlayerTest.java
@@ -1,49 +1,50 @@
 package orego.ladder;
+import orego.core.Coordinates;
 import orego.ui.Orego;
 
 import static orego.core.Colors.BLACK;
+import static orego.core.Colors.WHITE;
+import static orego.core.Coordinates.at;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class LadderPlayerTest {
 	private LadderPlayer player;
-	private Orego game;
 	
 	@Before
 	public void setUp() {
 		player = new LadderPlayer();
 	}
+	
 	@Test
 	public void testPlayLadder() {
-		String[] problem = {
+		player.reset();
+		String[] diagram = {
 				"...................",// 19
 				"...................",// 18
 				"...................",// 17
 				"...................",// 16
 				".....O.............",// 15
 				"....O#O............",// 14
-				"....O..............",// 13
+				"...................",// 13
 				"...................",// 12
 				"...................",// 11
 				"...................",// 10
 				"...................",// 9
-				"..#................",// 8
-				".#O#...............",// 7
-				".#.................",// 6
-				"...................",// 5
+				"...................",// 8
+				"...................",// 7
+				"...................",// 6
+				".............#.....",// 5
 				"...................",// 4
 				"...................",// 3
-				".....O.............",// 2
-				"..................."// 1
+				"...................",// 2
+				"..................."//  1
 	          // ABCDEFGHJKLMNOPQRST
 		};
-		
+		player.setUpProblem(WHITE, diagram);
+		player.getBoard().play(at("E13"));
 		System.out.println(player.getBoard());
-		
-		player.getBoard().setUpProblem(BLACK, problem);
-		//System.err.println(player.bestMove());
-		//game.handleCommand("genmove black");
+		System.out.println("Best move for black: " + Coordinates.pointToString(player.bestMove()));
 	}
-
 }
