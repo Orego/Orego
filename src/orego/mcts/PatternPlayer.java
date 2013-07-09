78736e0b46e7200cb0cd74156fee3d7e146587f1
Jason Galbraith <galbraith@maclabcs21.lclark.edu>
2013-07-09 13:04:42 -0700

    Changed patterns to include hash tables for both white and black.

--- src/orego/mcts/PatternPlayer.java
+++ src/orego/mcts/PatternPlayer.java
@@ -1,6 +1,6 @@
 package orego.mcts;
 
-import static orego.core.Board.NINE_PATTERN;
+import static orego.core.Board.*;
 import static orego.core.Colors.VACANT;
 import static orego.core.Coordinates.getAllPointsOnBoard;
 import static orego.core.Coordinates.pointToString;
@@ -12,45 +12,52 @@ import java.util.HashMap;
 import java.util.Set;
 import java.util.StringTokenizer;
 
+import orego.core.Board;
+import orego.core.Colors;
 import orego.patternanalyze.PatternInformation;
 
 public class PatternPlayer extends MctsPlayer {
-	
+
 	HashMap<Character, PatternInformation> threePatterns;
 	HashMap<Character, PatternInformation> fivePatterns;
 	HashMap<Character, PatternInformation> sevenPatterns;
 	HashMap<Character, PatternInformation> ninePatterns;
 	@SuppressWarnings("unchecked")
-	HashMap<Character, PatternInformation>[] patterns = new HashMap[4];
-	
+	HashMap<Character, PatternInformation>[][] patterns = new HashMap[4][2];
+
 	@SuppressWarnings("unchecked")
 	public PatternPlayer() {
-		for (int i = 0; i < NINE_PATTERN + 1; i++) {
-			//load from files
-			try {
-				ObjectInputStream ir = new ObjectInputStream(new FileInputStream(new File("../testFiles/patternPlayed"+(i*2+3)+".dat")));
-				patterns[i] = (HashMap<Character, PatternInformation>)(ir.readObject());
-				ir.close();
-			} catch(Exception ex) {
-				ex.printStackTrace();
+		for (int c = 0; c < 2; c++) {
+			for (int i = 0; i < NINE_PATTERN + 1; i++) {
+				// load from files
+				try {
+					ObjectInputStream ir = new ObjectInputStream(
+							new FileInputStream(new File(
+									"../testFiles/patternPlayed" + (i * 2 + 3)
+											+ Colors.colorToString(c) + ".dat")));
+					patterns[i][c] = (HashMap<Character, PatternInformation>) (ir
+							.readObject());
+					ir.close();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		new PatternPlayer();
 	}
-	
+
 	public PatternInformation getInformation(int patternType, char hash) {
-		PatternInformation toReturn = patterns[patternType].get(hash);
+		PatternInformation toReturn = patterns[patternType][getBoard().getColorToPlay()].get(hash);
 		if (toReturn != null) {
 			return toReturn;
-		}
-		else {
+		} else {
 			return new PatternInformation();
 		}
 	}
-	
+
 	@Override
 	public Set<String> getCommands() {
 		Set<String> result = super.getCommands();
@@ -58,10 +65,11 @@ public class PatternPlayer extends MctsPlayer {
 		result.add("gogui-pattern-info-5");
 		result.add("gogui-pattern-info-7");
 		result.add("gogui-pattern-info-9");
-//		result.add("gogui-pattern-win-rates-3");
-//		result.add("gogui-pattern-win-rates-5");
-//		result.add("gogui-pattern-win-rates-7");
-//		result.add("gogui-pattern-win-rates-9");
+		result.add("gogui-combined-pattern-info");
+		// result.add("gogui-pattern-win-rates-3");
+		// result.add("gogui-pattern-win-rates-5");
+		// result.add("gogui-pattern-win-rates-7");
+		// result.add("gogui-pattern-win-rates-9");
 		return result;
 	}
 
@@ -72,22 +80,26 @@ public class PatternPlayer extends MctsPlayer {
 		result.add("gfx/5x5 pattern info/gogui-pattern-info-5");
 		result.add("gfx/7x7 pattern info/gogui-pattern-info-7");
 		result.add("gfx/9x9 pattern info/gogui-pattern-info-9");
-//		result.add("gfx/Pattern win rates/gogui-pattern-win-rates-3");
-//		result.add("gfx/Pattern win rates/gogui-pattern-win-rates-5");
-//		result.add("gfx/Pattern win rates/gogui-pattern-win-rates-7");
-//		result.add("gfx/Pattern win rates/gogui-pattern-win-rates-9");
+		result.add("gfx/Combined pattern info/gogui-combined-pattern-info");
+		// result.add("gfx/Pattern win rates/gogui-pattern-win-rates-3");
+		// result.add("gfx/Pattern win rates/gogui-pattern-win-rates-5");
+		// result.add("gfx/Pattern win rates/gogui-pattern-win-rates-7");
+		// result.add("gfx/Pattern win rates/gogui-pattern-win-rates-9");
 		return result;
 	}
-	
+
 	@Override
 	public String handleCommand(String command, StringTokenizer arguments) {
 		boolean threadsWereRunning = threadsRunning();
 		stopThreads();
 		String result = null;
 		if (command.contains("gogui-pattern-info")) {
-			result = goguiPatternInfo((command.charAt(command.length()-1)-'0')/2-1);
-//		} else if (command.contains("gogui-pattern-win-rates")) {
-//			result = goguiPatternWinRates((command.charAt(command.length()-1)-'0')/2-1);
+			result = goguiPatternInfo((command.charAt(command.length() - 1) - '0') / 2 - 1);
+		} else if (command.contains("gogui-combined-pattern-info")) {
+			result = goguiCombinedPatternInfo();
+			// } else if (command.contains("gogui-pattern-win-rates")) {
+			// result =
+			// goguiPatternWinRates((command.charAt(command.length()-1)-'0')/2-1);
 		} else {
 			result = super.handleCommand(command, arguments);
 		}
@@ -99,32 +111,57 @@ public class PatternPlayer extends MctsPlayer {
 
 	private String goguiPatternInfo(int patternType) {
 		String result = "";
-		for (int p: getAllPointsOnBoard()){
-			if (getBoard().getColor(p) == VACANT){
-				double winRate = getWinRate(p);
-				if (winRate>0){
-					if (result.length()>0)
-						result += '\n';
-					PatternInformation info = getInformation(patternType,getBoard().getPatternHash(patternType, p));
-					result += String.format("COLOR %s %s\nLABEL %s %d",
-							colorCode(info.getRate()), pointToString(p),
-							pointToString(p), info.getRuns());
-				}
+		for (int p : getAllPointsOnBoard()) {
+			if (getBoard().getColor(p) == VACANT) {
+				if (result.length() > 0)
+					result += '\n';
+				PatternInformation info = getInformation(patternType,
+						getBoard().getPatternHash(patternType, p));
+				result += String.format("COLOR %s %s\nLABEL %s %d",
+						colorCode(info.getRate()), pointToString(p),
+						pointToString(p), info.getRuns());
 			}
 		}
 		return result;
 	}
-	
+
+	private String goguiCombinedPatternInfo() {
+		String result = "";
+		for (int p : getAllPointsOnBoard()) {
+			if (getBoard().getColor(p) == VACANT) {
+				if (result.length() > 0)
+					result += '\n';
+				float totalRate = 0;
+				int totalRuns = 0;
+				for (int i = 0; i < NINE_PATTERN + 1; i++) {
+					PatternInformation info = getInformation(i, getBoard()
+							.getPatternHash(i, p));
+					totalRate += info.getRate() * ((i + 1) * (i + 2) / 2);
+					totalRuns += info.getRuns();
+				}
+				totalRate /= 20.0f;
+				result += String.format("COLOR %s %s\nLABEL %s %d",
+						colorCode(totalRate), pointToString(p),
+						pointToString(p), totalRuns);
+			}
+		}
+		return result;
+	}
+
 	private String goguiPatternRuns(int patternType) {
 		String result = "";
-		for (int p: getAllPointsOnBoard()){
-			if (getBoard().getColor(p) == VACANT){
+		for (int p : getAllPointsOnBoard()) {
+			if (getBoard().getColor(p) == VACANT) {
 				double winRate = getWinRate(p);
-				if (winRate>0){
-					if (result.length()>0)
+				if (winRate > 0) {
+					if (result.length() > 0)
 						result += '\n';
-					result += String.format("LABEL %s %.0f%%",
-							pointToString(p), getInformation(patternType, getBoard().getPatternHash(patternType, p)).getRuns());
+					result += String.format(
+							"LABEL %s %.0f%%",
+							pointToString(p),
+							getInformation(patternType,
+									getBoard().getPatternHash(patternType, p))
+									.getRuns());
 				}
 			}
 		}
--- src/orego/patternanalyze/PatternCounterTwoPointOne.java
+++ src/orego/patternanalyze/PatternCounterTwoPointOne.java
@@ -15,45 +15,44 @@ import java.util.Set;
 import java.util.StringTokenizer;
 
 import orego.core.Board;
+import orego.core.Colors;
 import orego.util.IntSet;
 
-
 /**
- * This class looks only at patterns that were actually played.
- * These are sorted based on the ratio of patterns seen to patterns played.
+ * This class looks only at patterns that were actually played. These are sorted
+ * based on the ratio of patterns seen to patterns played.
+ * 
  * @author galbraith
  */
-
 
 public class PatternCounterTwoPointOne {
 
 	/**
 	 * The number of total patterns, including impossible ones.
 	 */
-//	public static final int NUMBER_OF_NEIGHBORHOODS = Character.MAX_VALUE + 1;
+	// public static final int NUMBER_OF_NEIGHBORHOODS = Character.MAX_VALUE +
+	// 1;
 
-//	private static final int PATTERN_LENGTH = 24;
-	
-//	private static final int PATTERN_STORAGE_CUTOFF = 500000;
-//	private static final int PATTERNS_TO_REMOVE = PATTERN_STORAGE_CUTOFF / 2;
-	
-//	private static final int PATTERNS_TO_KEEP = (int)Math.pow(2, 16);
-	
-//	private static final int SORTED_ARRAY_PATTERN = 0;
-//	private static final int SORTED_ARRAY_PLAYED = 1;
-	
+	// private static final int PATTERN_LENGTH = 24;
+
+	// private static final int PATTERN_STORAGE_CUTOFF = 500000;
+	// private static final int PATTERNS_TO_REMOVE = PATTERN_STORAGE_CUTOFF / 2;
+
+	// private static final int PATTERNS_TO_KEEP = (int)Math.pow(2, 16);
+
+	// private static final int SORTED_ARRAY_PATTERN = 0;
+	// private static final int SORTED_ARRAY_PLAYED = 1;
+
 	private static final boolean DEBUG = false;
 
 	/**
-	 * Long[] will have:
-	 * 0: frequency seen
-	 * 1: largest turn
-	 * 2: lowest turn
-	 * 3: total of all turns
+	 * Long[] will have: 0: frequency seen 1: largest turn 2: lowest turn 3:
+	 * total of all turns
 	 * */
-	private static HashMap<Character, PatternInformation>[] patternSeen;
-	private static String TEST_DIRECTORY = "../../../Desktop/Test Games/";
-	//private static String TEST_DIRECTORY = "../../Test Games/";
+	private static HashMap<Character, PatternInformation>[][] patternSeen;
+	private static String TEST_DIRECTORY = "../../Test Games/kgs-19-2001/";
+
+	// private static String TEST_DIRECTORY = "../../Test Games/";
 
 	public static void main(String[] args) {
 		new PatternCounterTwoPointOne();
@@ -62,40 +61,59 @@ public class PatternCounterTwoPointOne {
 	@SuppressWarnings("unchecked")
 	public PatternCounterTwoPointOne() {
 		try {
-			patternSeen = new HashMap[4];
-			for (int i = 0; i < NINE_PATTERN+1; i++) {
-				patternSeen[i] = new HashMap<Character, PatternInformation>();
+			patternSeen = new HashMap[4][2];
+			for (int i = 0; i < NINE_PATTERN + 1; i++) {
+				for (int j = 0; j < 2; j++) {
+					patternSeen[i][j] = new HashMap<Character, PatternInformation>();
+				}
 			}
 			setUp(TEST_DIRECTORY);
 			PrintWriter bw = new PrintWriter(new FileWriter(new File(
 					TEST_DIRECTORY + "outputPlayed.txt")));
 			String output = "";
-			for (int i = 0; i < NINE_PATTERN + 1; i++) {
-				Set<Character> initialPatternSeen = patternSeen[i].keySet();
-				output += "Pattern:" + (i*2+3) + " below.\n";
-				bw.write(output);
-				System.out.println("Total patterns:" + initialPatternSeen.size());
-				for (Character pattern : initialPatternSeen) {		
-					if (patternSeen[i].get(pattern) != null) {
-						output = " Total Wins:" + patternSeen[i].get(pattern).getRate() * patternSeen[i].get(pattern).getRuns();
-						output += " Total Runs:" + patternSeen[i].get(pattern).getRuns();
-						output += " Win Rate:" + patternSeen[i].get(pattern).getRate();
-						output += " "+(i*2+3)+"\n";
-						output += patternSeen[i].get(pattern).toString();
-						bw.write(output);
+			for (int c = 0; c < 2; c++) {
+				output = "Color: " + Colors.colorToString(c) + " below.";
+				for (int i = 0; i < NINE_PATTERN + 1; i++) {
+					Set<Character> initialPatternSeen = patternSeen[i][c]
+							.keySet();
+					output = "Pattern:" + (i * 2 + 3) + " below.\n";
+					bw.write(output);
+					System.out.println("Total patterns:"
+							+ initialPatternSeen.size());
+					for (Character pattern : initialPatternSeen) {
+						if (patternSeen[i][c].get(pattern) != null) {
+							output = " Total Wins:"
+									+ patternSeen[i][c].get(pattern).getRate()
+									* patternSeen[i][c].get(pattern).getRuns();
+							output += " Total Runs:"
+									+ patternSeen[i][c].get(pattern).getRuns();
+							output += " Win Rate:"
+									+ patternSeen[i][c].get(pattern).getRate();
+							output += " " + (i * 2 + 3) + "\n";
+							output += patternSeen[i][c].get(pattern).toString();
+							bw.write(output);
+						}
 					}
 				}
 			}
 			System.out.println("Done.");
 			bw.write(output);
-			System.out.println("Written to file "+TEST_DIRECTORY + "outputPlayed.txt");
+			System.out.println("Written to file " + TEST_DIRECTORY
+					+ "outputPlayed.txt");
 			bw.close();
-			for (int i = 0; i < NINE_PATTERN + 1; i++) {
-				ObjectOutputStream ow = new ObjectOutputStream(new FileOutputStream(new File(TEST_DIRECTORY + "patternPlayed"+(i*2+3)+".dat")));
-				ow.writeObject(patternSeen[i]);
-				ow.flush();
-				ow.close();
-				System.out.println("Written to file "+TEST_DIRECTORY + "patternPlayed"+(i*2+3)+".dat");
+			for (int c = 0; c < 2; c++) {
+				for (int i = 0; i < NINE_PATTERN + 1; i++) {
+					ObjectOutputStream ow = new ObjectOutputStream(
+							new FileOutputStream(new File(TEST_DIRECTORY
+									+ "patternPlayed" + (i * 2 + 3)
+									+ Colors.colorToString(c) + ".dat")));
+					ow.writeObject(patternSeen[i][c]);
+					ow.flush();
+					ow.close();
+					System.out.println("Written to file " + TEST_DIRECTORY
+							+ "patternPlayed" + (i * 2 + 3)
+							+ Colors.colorToString(c) + ".dat");
+				}
 			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
@@ -109,36 +127,25 @@ public class PatternCounterTwoPointOne {
 	 * 
 	 * @return
 	 */
-	/*public PatternInformation[] sortHashMapIntoArray() {
-		PatternInformation[] toReturn = new PatternInformation[NINE_PATTERN + 1];
-		for (int i = 0; i < NINE_PATTERN + 1; i++) {		
-			Set<Character> patterns = patternSeen[i].keySet();
-			PatternInformation[] sortedArray = new PatternInformation[patterns.size()];
-			int index = 0; 
-			for(char pattern : patterns) {
-				sortedArray[index] = patternSeen[i].get(pattern);
-				index++;
-			}
-			for (int j = 0; j < sortedArray.length - 1; j++) {
-				int maxindex = j+1;
-				double maxvalue = (sortedArray[j].getRuns());
-				for (int k = j+1; k < sortedArray.length; k++) {
-					if (sortedArray[k].getRuns() > maxvalue) {
-						maxindex = k;
-						maxvalue = sortedArray[k].getRuns();
-					}
-				}
-				PatternInformation swapValue = sortedArray[maxindex];
-				sortedArray[maxindex] = sortedArray[j];
-				sortedArray[j] = swapValue;
-			}
-			toReturn = sortedArray;
-		}
-		return toReturn;
-	}*/
+	/*
+	 * public PatternInformation[] sortHashMapIntoArray() { PatternInformation[]
+	 * toReturn = new PatternInformation[NINE_PATTERN + 1]; for (int i = 0; i <
+	 * NINE_PATTERN + 1; i++) { Set<Character> patterns =
+	 * patternSeen[i].keySet(); PatternInformation[] sortedArray = new
+	 * PatternInformation[patterns.size()]; int index = 0; for(char pattern :
+	 * patterns) { sortedArray[index] = patternSeen[i].get(pattern); index++; }
+	 * for (int j = 0; j < sortedArray.length - 1; j++) { int maxindex = j+1;
+	 * double maxvalue = (sortedArray[j].getRuns()); for (int k = j+1; k <
+	 * sortedArray.length; k++) { if (sortedArray[k].getRuns() > maxvalue) {
+	 * maxindex = k; maxvalue = sortedArray[k].getRuns(); } } PatternInformation
+	 * swapValue = sortedArray[maxindex]; sortedArray[maxindex] =
+	 * sortedArray[j]; sortedArray[j] = swapValue; } toReturn = sortedArray; }
+	 * return toReturn; }
+	 */
 
 	/**
-	 * Takes a directory of SGF files and walks through them, counting how often patterns are seen and played.
+	 * Takes a directory of SGF files and walks through them, counting how often
+	 * patterns are seen and played.
 	 */
 	public void setUp(String filepath) {
 		try {
@@ -152,8 +159,9 @@ public class PatternCounterTwoPointOne {
 					setUp(filename);
 				} else if (dirList[i].toLowerCase().endsWith(".sgf")) {
 					checkPatterns(file);
-					//if (DEBUG) System.out.println("Number of patterns:" + patternSeen.size() + " Game:" + i);
-					//clearOutNonsense();
+					// if (DEBUG) System.out.println("Number of patterns:" +
+					// patternSeen.size() + " Game:" + i);
+					// clearOutNonsense();
 				}
 			}
 		} catch (Exception e) {
@@ -161,46 +169,41 @@ public class PatternCounterTwoPointOne {
 			System.exit(1);
 		}
 	}
-	
-	/** 
-	 * Gets rid of uncommon patterns in the hashmap to alleviate memory issues.
-	 */
-	/*public void clearOutNonsense() {
-		if (patternSeen.size() > PATTERN_STORAGE_CUTOFF) {
-			int threshold = 1;
-			while (patternSeen.size() > PATTERNS_TO_REMOVE) {
-				removePatterns(patternSeen.size() - PATTERNS_TO_REMOVE, threshold++);
-			}
-		}
-	}*/
 
 	/**
-	 * Remove patterns seen only once.  If toRemove is -1 it will remove all patterns seen once.
-	 * @param Number of patterns to remove.
+	 * Gets rid of uncommon patterns in the hashmap to alleviate memory issues.
+	 */
+	/*
+	 * public void clearOutNonsense() { if (patternSeen.size() >
+	 * PATTERN_STORAGE_CUTOFF) { int threshold = 1; while (patternSeen.size() >
+	 * PATTERNS_TO_REMOVE) { removePatterns(patternSeen.size() -
+	 * PATTERNS_TO_REMOVE, threshold++); } } }
+	 */
+
+	/**
+	 * Remove patterns seen only once. If toRemove is -1 it will remove all
+	 * patterns seen once.
+	 * 
+	 * @param Number
+	 *            of patterns to remove.
 	 * @return
 	 */
-	/*private int removePatterns(int toRemove, int threshold) {
-		int removed = 0;
-		Set<Character> patterns = patternSeen.keySet();
-		Object[] keys = patterns.toArray();
-		for(Object key : keys) {
-			Long pattern = (Long)key;
-			if (patternSeen.get(pattern)[PATTERN_PLAYED] <= threshold) {
-				patternSeen.remove(pattern);
-				removed++;
-				if (removed >= toRemove && toRemove != -1){
-					break;
-				}
-			}
-		}
-		if (DEBUG) System.out.println("Removed single patterns:" + removed + " of size:"+threshold);
-		return removed;
-	}*/
+	/*
+	 * private int removePatterns(int toRemove, int threshold) { int removed =
+	 * 0; Set<Character> patterns = patternSeen.keySet(); Object[] keys =
+	 * patterns.toArray(); for(Object key : keys) { Long pattern = (Long)key; if
+	 * (patternSeen.get(pattern)[PATTERN_PLAYED] <= threshold) {
+	 * patternSeen.remove(pattern); removed++; if (removed >= toRemove &&
+	 * toRemove != -1){ break; } } } if (DEBUG)
+	 * System.out.println("Removed single patterns:" + removed +
+	 * " of size:"+threshold); return removed; }
+	 */
 
-	
-	/** 
+	/**
 	 * Check for the patterns in a particular file.
-	 * @param SGF file of a particular game.
+	 * 
+	 * @param SGF
+	 *            file of a particular game.
 	 */
 	public void checkPatterns(File dir) {
 		try {
@@ -235,29 +238,45 @@ public class PatternCounterTwoPointOne {
 				int nextPlay = board.getMove(currentTurn);
 				do {
 					possibleMoves = patternBoard.getVacantPoints();
-					randomMove = possibleMoves.get((int)(Math.random()*possibleMoves.size()));
-				} while (!patternBoard.isLegal(randomMove) || randomMove == nextPlay);
+					randomMove = possibleMoves
+							.get((int) (Math.random() * possibleMoves.size()));
+				} while (!patternBoard.isLegal(randomMove)
+						|| randomMove == nextPlay);
 				if (isOnBoard(nextPlay)) {
 					for (int patternType = 0; patternType < NINE_PATTERN + 1; patternType++) {
-						char lastPlayHash = patternBoard.getPatternHash(patternType, nextPlay);
-						if (patternSeen[patternType].containsKey(lastPlayHash)) {
-							PatternInformation patternData = patternSeen[patternType].get(lastPlayHash);
-							float totalWins = (patternData.getRate() * patternData.getRuns());
+						char lastPlayHash = patternBoard.getPatternHash(
+								patternType, nextPlay);
+						if (patternSeen[patternType][patternBoard.getColorToPlay()].containsKey(lastPlayHash)) {
+							PatternInformation patternData = patternSeen[patternType][patternBoard.getColorToPlay()]
+									.get(lastPlayHash);
+							float totalWins = (patternData.getRate() * patternData
+									.getRuns());
 							patternData.setRuns(patternData.getRuns() + 1);
-							patternData.setRate((totalWins+1) / patternData.getRuns());
+							patternData.setRate((totalWins + 1)
+									/ patternData.getRuns());
+						} else {
+							patternSeen[patternType][patternBoard.getColorToPlay()].put(
+									lastPlayHash,
+									new PatternInformation(1.0f, 1,
+											patternBoard.printPattern(
+													patternType, nextPlay)));
 						}
-						else {
-							patternSeen[patternType].put(lastPlayHash, new PatternInformation(1.0f, 1, patternBoard.printPattern(patternType, nextPlay)));
-						}
-						char moveHash = patternBoard.getPatternHash(patternType, randomMove);
-						if (patternSeen[patternType].containsKey(moveHash)) {
-							PatternInformation patternData = patternSeen[patternType].get(moveHash);
-							float totalWins = (patternData.getRate() * patternData.getRuns());
+						char moveHash = patternBoard.getPatternHash(
+								patternType, randomMove);
+						if (patternSeen[patternType][patternBoard.getColorToPlay()].containsKey(moveHash)) {
+							PatternInformation patternData = patternSeen[patternType][patternBoard.getColorToPlay()]
+									.get(moveHash);
+							float totalWins = (patternData.getRate() * patternData
+									.getRuns());
 							patternData.setRuns(patternData.getRuns() + 1);
-							patternData.setRate((totalWins) / patternData.getRuns());
-						}
-						else {
-							patternSeen[patternType].put(moveHash, new PatternInformation(0.0f, 1, patternBoard.printPattern(patternType, randomMove)));
+							patternData.setRate((totalWins)
+									/ patternData.getRuns());
+						} else {
+							patternSeen[patternType][patternBoard.getColorToPlay()].put(
+									moveHash,
+									new PatternInformation(0.0f, 1,
+											patternBoard.printPattern(
+													patternType, randomMove)));
 						}
 					}
 				}
