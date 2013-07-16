5a73af1e4f1f11beba8a8d7fadc25b49310ae64b
Alex Terenin <aterenin@umail.ucsb.edu>
2013-07-16 15:55:01 -0700

    git pattern heusteric

---
+++ src/orego/heuristic/DeepPatternHeuristicTest.java
@@ -0,0 +1,14 @@
+package orego.heuristic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DeepPatternHeuristicTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
