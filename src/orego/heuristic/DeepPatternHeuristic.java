939330b8c5c1b30ff7d268b1f2f036d292830dda
Alex Terenin <aterenin@umail.ucsb.edu>
2013-07-17 10:46:47 -0700

    deep pattern heusteric

--- src/orego/heuristic/DeepPatternHeuristic.java
+++ src/orego/heuristic/DeepPatternHeuristic.java
@@ -10,75 +10,59 @@ import orego.core.Colors;
 import orego.patternanalyze.PatternInformation;
 import orego.util.IntSet;
 
+@SuppressWarnings("unchecked")
 public class DeepPatternHeuristic extends Heuristic{
 	
-	@SuppressWarnings("unchecked")
-	private HashMap<Character, PatternInformation>[][] patterns = new HashMap[4][2];
+	// TODO Use a direct-addressing table instead of a hash table
+	public static final HashMap<Character, PatternInformation>[][] PATTERNS = new HashMap[4][2];
 	
 	private double[] patternWeights;
 	
-	private double goodPatternThreshold = .75;
+	public static final double GOOD_PATTERN_THRESHOLD = .75;
 		
 	public DeepPatternHeuristic(int weight) {
 		super(weight);
 		patternWeights = new double[] { 1 / 20.0, 3 / 20.0, 6 / 20.0, 10 / 20.0 };
-		loadPatternHashMaps();
 	}
 	
 	public void prepare(Board board, boolean local){
 		super.prepare(board, local);
-		setWeights(9 * board.getTurn() / 220.0 + 1, 
-				3 * board.getTurn() / 220.0 + 3,
-				3 * (1 - board.getTurn() / 220.0) + 3, 
-				9 * (1 - board.getTurn() / 220.0) + 1);
-		IntSet moves = board.getVacantPoints();
-		for (int i = 0; i < moves.size(); i++) {
-			if (board.isFeasible(moves.get(i))) {
-				float tempRate = (float) getWinRate(board, moves.get(i));
-				if (tempRate > goodPatternThreshold) {
-					if(board.isLegal(moves.get(i))){
-						recommend(moves.get(i));
+		if (!local) {
+			// Update weights to pay more attention to smaller patterns later in game
+			patternWeights[0] = 9 * board.getTurn() / 220.0 + 1;
+			patternWeights[1] = 3 * board.getTurn() / 220.0 + 3;
+			patternWeights[2] = 3 * (1 - board.getTurn() / 220.0) + 3;
+			patternWeights[3] = 9 * (1 - board.getTurn() / 220.0) + 1;
+			double sum = 0;
+			for (int i = 0; i < patternWeights.length; i++) {
+				sum += patternWeights[i];
+			}
+			for (int i = 0; i < patternWeights.length; i++) {
+				patternWeights[i] /= sum;
+			}
+			IntSet moves = board.getVacantPoints();
+			for (int i = 0; i < moves.size(); i++) {
+				if (board.isFeasible(moves.get(i))) {
+					float tempRate = (float) getWinRate(board, moves.get(i));
+					if (tempRate > GOOD_PATTERN_THRESHOLD) {
+						if(board.isLegal(moves.get(i))){
+							recommend(moves.get(i));
+						}
 					}
 				}
 			}
 		}
-	}
+	}	
 	
-	private double patternWeight(int pattern) {
-		return patternWeights[pattern];
-	}
-	
-	public void setWeights(double threePattern, double fivePattern,
-			double sevenPattern, double ninePattern) {
-		double sum = threePattern + fivePattern + sevenPattern + ninePattern;
-		patternWeights = new double[] { threePattern / sum, fivePattern / sum,
-				sevenPattern / sum, ninePattern / sum };
-	}
-	
-	
-	private float getWinRate(Board b, int point) {
-		float tempRate = 0;
+	public float getWinRate(Board b, int point) {
+		float result = 0;
 		for (int pattern = 0; pattern <= NINE_PATTERN; pattern++) {
-			PatternInformation info = getInformation(pattern,
-					b.getPatternHash(pattern, point), b.getColorToPlay());
-			tempRate += info.getRate() * patternWeight(pattern);
+			result += PATTERNS[pattern][b.getColorToPlay()].get(b.getPatternHash(pattern, point)).getRate() * patternWeights[pattern];
 		}
-		return tempRate;
+		return result;
 	}
-	
-	public PatternInformation getInformation(int patternType, char hash,
-			int color) {
-		PatternInformation toReturn = patterns[patternType][color].get(hash);
 
-		if (toReturn != null) {
-			return toReturn;
-		} else {
-			return new PatternInformation();
-		}
-	}
-	
-	@SuppressWarnings("unchecked")
-	private void loadPatternHashMaps() {
+	static {
 		for (int c = 0; c < 2; c++) {
 			for (int i = 0; i < NINE_PATTERN + 1; i++) {
 				// load from files
@@ -87,7 +71,7 @@ public class DeepPatternHeuristic extends Heuristic{
 							new FileInputStream(new File(
 									"./testFiles/patternPlayed" + (i * 2 + 3)
 											+ Colors.colorToString(c) + ".dat")));
-					patterns[i][c] = (HashMap<Character, PatternInformation>) (ir
+					PATTERNS[i][c] = (HashMap<Character, PatternInformation>) (ir
 							.readObject());
 					ir.close();
 				} catch (Exception ex) {
