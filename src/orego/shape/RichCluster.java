f86f665633f861122566d04a826df790b7bc0700
Peter Drake <drake@lclark.edu>
2013-07-24 09:49:59 -0700

    Using reply1 hash in RichCluster.

--- src/orego/shape/RichCluster.java
+++ src/orego/shape/RichCluster.java
@@ -12,12 +12,12 @@ public class RichCluster extends Cluster {
 	private Table[] global;
 	
 	/** Table for responses to the last move, indexed by color to move. */
-//	private Table[] reply1;
+	private Table[] reply1;
 	
 	public RichCluster(int tables, int bits) {
 		super(tables, bits);
 		global = new Table[] {new Table(tables, bits), new Table(tables, bits)};
-//		reply1 = new Table[] {new Table(tables, bits), new Table(tables, bits)};
+		reply1 = new Table[] {new Table(tables, bits), new Table(tables, bits)};
 	}
 
 	@Override
@@ -25,7 +25,7 @@ public class RichCluster extends Cluster {
 		super.setCount(count);
 		for (int color = BLACK; color <= WHITE; color++) {
 			global[color].setCount(count);
-//			reply1[color].setCount(count);
+			reply1[color].setCount(count);
 		}
 	}
 
@@ -33,9 +33,9 @@ public class RichCluster extends Cluster {
 	public long getCount(Board board, int move) {
 		int color = board.getColorToPlay();
 		long globalHash = board.getHash() ^ Board.ZOBRIST_HASHES[color][move];
-//		long reply1Hash = Board.ZOBRIST_HASHES[opposite(color)][board.getMove(board.getTurn() - 1)] ^ Board.ZOBRIST_HASHES[color][move];
-//		return (super.getCount(board, move) + global[color].getCount(globalHash) + reply1[color].getCount(reply1Hash)) / 3;
-		return (super.getCount(board, move) + global[color].getCount(globalHash)) / 2;
+		long reply1Hash = Board.ZOBRIST_HASHES[opposite(color)][board.getMove(board.getTurn() - 1)] ^ Board.ZOBRIST_HASHES[color][move];
+		return (super.getCount(board, move) + global[color].getCount(globalHash) + reply1[color].getCount(reply1Hash)) / 3;
+//		return (super.getCount(board, move) + global[color].getCount(globalHash)) / 2;
 	}
 
 	@Override
@@ -44,17 +44,17 @@ public class RichCluster extends Cluster {
 		int color = board.getColorToPlay();
 		long globalHash = board.getHash() ^ Board.ZOBRIST_HASHES[color][move];
 		global[color].store(globalHash, win);
-//		long reply1Hash = Board.ZOBRIST_HASHES[opposite(color)][board.getMove(board.getTurn() - 1)] ^ Board.ZOBRIST_HASHES[color][move];
-//		reply1[color].store(reply1Hash, win);
+		long reply1Hash = Board.ZOBRIST_HASHES[opposite(color)][board.getMove(board.getTurn() - 1)] ^ Board.ZOBRIST_HASHES[color][move];
+		reply1[color].store(reply1Hash, win);
 	}
 
 	@Override
 	public float getWinRate(Board board, int move) {
 		int color = board.getColorToPlay();
 		long globalHash = board.getHash() ^ Board.ZOBRIST_HASHES[color][move];
-//		long reply1Hash = Board.ZOBRIST_HASHES[opposite(color)][board.getMove(board.getTurn() - 1)] ^ Board.ZOBRIST_HASHES[color][move];
-//		return (super.getWinRate(board, move) + global[color].getWinRate(globalHash) + reply1[color].getWinRate(reply1Hash)) / 3.0f;
-		return (super.getWinRate(board, move) + global[color].getWinRate(globalHash)) / 2.0f;
+		long reply1Hash = Board.ZOBRIST_HASHES[opposite(color)][board.getMove(board.getTurn() - 1)] ^ Board.ZOBRIST_HASHES[color][move];
+		return (super.getWinRate(board, move) + global[color].getWinRate(globalHash) + reply1[color].getWinRate(reply1Hash)) / 3.0f;
+//		return (super.getWinRate(board, move) + global[color].getWinRate(globalHash)) / 2.0f;
 	}
 
 }
