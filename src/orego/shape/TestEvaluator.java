401c69e4f0ee2ea7e040ff9cd638ad0bcc066bd3
Laura Vonessen <lvonessen@email.arizona.edu>
2013-07-30 15:53:34 -0700

    Can now print index values prettily.

--- src/orego/shape/TestEvaluator.java
+++ src/orego/shape/TestEvaluator.java
@@ -10,6 +10,8 @@ import java.util.StringTokenizer;
  */
 public class TestEvaluator {
 
+	private int[][][][][] array; //[threshold][initNoise][finalNoise][decay][cutoff]
+	
 	public static void main(String[] args) {
 //		new TestEvaluator().run(orego.experiment.Debug.OREGO_ROOT_DIRECTORY + "SgfTestFiles" + File.separator);
 		new TestEvaluator().run("/Network/Servers/maccsserver.lclark.edu/Users/lvonessen/Desktop/cgtc-2.0.1");
@@ -20,16 +22,64 @@ public class TestEvaluator {
 	 * for the number of tests that passed
 	 */
 	public void run(String directory) {
+		array = new int[3][2][3][3][3];
 		for (int i = 0; i < 70; i++) {
 			try {
 				File file = new File(directory + File.separator + "index" + i
 						+ ".html");
 				int passes = lookForPasses(file);
 				System.out.println(i + " " + passes);
+				store(i,passes);
 			} catch (Exception e) {
 				System.out.println(i + " is missing");
 			}
 		}
+		printArray();
+	}
+
+	private void printArray() {
+		// [threshold][initNoise][finalNoise][decay][cutoff]
+		System.out.println("            thr         thr         thr\n" +
+						   "            cut cut cut cut cut cut cut cut cut");
+		for (int initNoise = 0; initNoise < 2; initNoise++) {
+			System.out.print("beg ");
+			for (int finalNoise = 0; finalNoise < 3; finalNoise++) {
+				if (finalNoise != 0){
+					System.out.print("    ");
+				}
+				System.out.print("fin ");
+				for (int decay = 0; decay < 3; decay++) {
+					if (decay != 0){
+						System.out.print("        ");
+					}
+					System.out.print("dec ");
+					for (int threshold = 0; threshold < 3; threshold++) {
+						for (int cutoff = 0; cutoff < 3; cutoff++) {
+							if (array[threshold][initNoise][finalNoise][decay][cutoff] == 0)
+								System.out.print("--- ");
+							else
+								System.out
+									.print(array[threshold][initNoise][finalNoise][decay][cutoff]
+											+ " ");
+						}
+					}
+					System.out.println();
+				}
+			}
+		}
+	}
+
+	private void store(int index, int passes) {
+		//array[threshold][initNoise][finalNoise][decay][cutoff]
+		int threshold = index/54;
+		index%=54;
+		int initialNoise = index/27;
+		index%=27;
+		int finalNoise = index/9;
+		index%=9;
+		int noiseDecay = index/3;
+		int cutOff = index%3;
+		array[threshold][initialNoise][finalNoise][noiseDecay][cutOff] = passes;
 	}
 
 	/**
@@ -49,6 +99,7 @@ public class TestEvaluator {
 			s.nextLine();
 		}
 		StringTokenizer stoken = new StringTokenizer(s.nextLine(),"<>td/");
+		s.close();
 		return Integer.parseInt(stoken.nextToken());
 	}
 }
