6e9d02d28ed012d89c648c94562ff158e1d129f2
Samuel Kelly <samkelly@lclark.edu>
2013-07-31 15:24:15 -0700

    Designed yet another short experiment.

--- src/orego/experiment/ExperimentConfiguration.java
+++ src/orego/experiment/ExperimentConfiguration.java
@@ -65,7 +65,7 @@ public class ExperimentConfiguration {
 			/ (2 * HOSTS.length * GAMES_PER_HOST);
 
 	/** The amount of time each player is allocated for each game. */
-	public static final int GAME_TIME_IN_SECONDS = 200;
+	public static final int GAME_TIME_IN_SECONDS = 500;
 	
 	static {
 		assert 2 * HOSTS.length * GAMES_PER_HOST * GAMES_PER_COLOR == GAMES_PER_CONDITION : "Games per condition must be a multiple of 2 * <# of hosts> * <games per host>";
@@ -75,39 +75,13 @@ public class ExperimentConfiguration {
 	 * Command line arguments to Orego for the various conditions in the
 	 * experiment.
 	 */
-	public static final String[] CONDITIONS = new String[2];
+	public static final String[] CONDITIONS = new String[4];
 
 	static {
-		CONDITIONS[0] = "threads=2 book=FusekiBook";
-		CONDITIONS[1] = "threads=2 book=FusekiBook compare-rest compare-rest-conf=0.99 early-exit-mult=1.5";
-//		CONDITIONS[0] = "threads=2 book=FusekiBook compare-rest compare-rest-conf=0.95 early-exit-mult=1.0 rollover-coefficient=1.0 max-time-per-move-msec=7000";
-//		CONDITIONS[0] = "threads=2 book=FusekiBook compare-rest compare-rest-conf=0.95 early-exit-mult=1.5 rollover-coefficient=0.6";
-//		CONDITIONS[1] = "threads=2 book=FusekiBook compare-rest compare-rest-conf=0.95 early-exit-mult=1.50 roll-over-time=true";
-//		CONDITIONS[2] = "threads=2 book=FusekiBook compare-rest compare-rest-conf=0.95 early-exit-mult=1.75 roll-over-time=true";
-//		CONDITIONS[3] = "threads=2 book=FusekiBook compare-rest compare-rest-conf=0.95 early-exit-mult=2.00 roll-over-time=true";
-//		CONDITIONS[1] = "threads=2 book=FusekiBook compare-rest compare-rest-conf=0.99 early-exit-mult=1.5";
-//		CONDITIONS[2] = "threads=2 book=FusekiBook compare-rest compare-rest-conf=0.99 early-exit-mult=1.75";
-//		CONDITIONS[3] = "threads=2 book=FusekiBook compare-rest compare-rest-conf=0.99 early-exit-mult=2.0";
-//		CONDITIONS[4] = "threads=2 book=FusekiBook compare-rest compare-rest-conf=0.95 early-exit-mult=1.0";
-//		CONDITIONS[5] = "threads=2 book=FusekiBook compare-rest compare-rest-conf=0.95 early-exit-mult=1.5";
-//		CONDITIONS[6] = "threads=2 book=FusekiBook compare-rest compare-rest-conf=0.95 early-exit-mult=1.75";
-//		CONDITIONS[7] = "threads=2 book=FusekiBook compare-rest compare-rest-conf=0.95 early-exit-mult=2.0";
-//		CONDITIONS[8] = "threads=2 book=FusekiBook compare-second compare-second-conf=0.85 early-exit-mult=1.0";
-//		CONDITIONS[9] = "threads=2 book=FusekiBook compare-second compare-second-conf=0.85 early-exit-mult=1.5";
-//		CONDITIONS[10] = "threads=2 book=FusekiBook compare-second compare-second-conf=0.85 early-exit-mult=1.75";
-//		CONDITIONS[11] = "threads=2 book=FusekiBook compare-second compare-second-conf=0.85 early-exit-mult=2.0";
-//		CONDITIONS[12] = "threads=2 book=FusekiBook compare-second compare-second-conf=0.75 early-exit-mult=1.0";
-//		CONDITIONS[13] = "threads=2 book=FusekiBook compare-second compare-second-conf=0.75 early-exit-mult=1.5";
-//		CONDITIONS[14] = "threads=2 book=FusekiBook compare-second compare-second-conf=0.75 early-exit-mult=1.75";
-//		CONDITIONS[15] = "threads=2 book=FusekiBook compare-second compare-second-conf=0.75 early-exit-mult=2.0";
-//		CONDITIONS[16] = "threads=2 book=FusekiBook benefit-from-previous-work early-exit-mult=1.0";
-//		CONDITIONS[17] = "threads=2 book=FusekiBook benefit-from-previous-work early-exit-mult=1.5";
-//		CONDITIONS[18] = "threads=2 book=FusekiBook benefit-from-previous-work early-exit-mult=2.0";
-//		CONDITIONS[19] = "threads=2 book=FusekiBook quick-moves-out-of-book=3";
-//		CONDITIONS[20] = "threads=2 book=FusekiBook quick-moves-out-of-book=5";
-//		CONDITIONS[21] = "threads=2 book=FusekiBook quick-moves-out-of-book=7";
-//		CONDITIONS[22] = "threads=2 book=FusekiBook early-c early-exit-mult=2.0";
-//		CONDITIONS[23] = "threads=2 book=FusekiBook";
+		CONDITIONS[0] = "threads=2 book=FusekiBook compare-rest compare-rest-conf=0.95 early-exit-mult=1.0 rollover-coefficient=0.4 max-time-per-move-msec=7000";
+		CONDITIONS[1] = "threads=2 book=FusekiBook compare-rest compare-rest-conf=0.95 early-exit-mult=1.5 rollover-coefficient=0.4 max-time-per-move-msec=7000";
+		CONDITIONS[2] = "threads=2 book=FusekiBook compare-rest compare-rest-unconf=0.75 compare-rest-unconf-mult=0.5";
+		CONDITIONS[3] = "threads=2 book=FusekiBook compare-second compare-second-unconf=0.50 compare-second-unconf-mult=0.5";
 	}
 	
 	/** Path to run gnugo on your machine */
