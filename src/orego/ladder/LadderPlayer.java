9756cfb8dc02d2efeac909da50fcadd098199de6
Sam Levenick <slevenick@lclark.edu>
2013-08-02 11:23:38 -0700

    Finished? LadderPlayer, it passes all tests and works for groups with two liberties as well as one.

--- src/orego/ladder/LadderPlayer.java
+++ src/orego/ladder/LadderPlayer.java
@@ -58,20 +58,26 @@ public class LadderPlayer extends TimePlayer {
 		// liberty has exactly two vacant neighbors
 		IntSet chainsInAtari = getBoard().getChainsInAtari(color);
 		IntSet libertiesOfLadders = new IntSet(getFirstPointBeyondBoard());
-		for (int i = 0; i < chainsInAtari.size(); i++) {
-			int liberty = getBoard().getLibertyOfChainInAtari(
-					chainsInAtari.get(i));
-			if (getBoard().getVacantNeighborCount(liberty) == 2) {
-				libertiesOfLadders.add(liberty);
-			}
+		for(int i=0; i<chainsInAtari.size(); i++){
+			libertiesOfLadders.add(chainsInAtari.get(i));
 		}
+		// for (int i = 0; i < chainsInAtari.size(); i++) {
+		// int liberty = getBoard().getLibertyOfChainInAtari(
+		// chainsInAtari.get(i));
+		// if (getBoard().getVacantNeighborCount(liberty) == 2) {
+		// libertiesOfLadders.add(liberty);
+		// }
+		// }
+		// return libertiesOfLadders;
+		
+		
 		return libertiesOfLadders;
 	}
-	
+
 	public IntSet twoLibertyGroups(int color) {
 		IntSet libertiesOfTwoLadders = new IntSet(getFirstPointBeyondBoard());
 		for (int i = 0; i < Coordinates.getBoardArea(); i++) {
-			if (getBoard().getColor(i)==color) {
+			if (getBoard().getColor(i) == color) {
 				if (getBoard().getChainId(i) == i) {
 					if (getBoard().getLibertyCount(i) == 2) {
 						System.err.println("ADDING");
@@ -97,72 +103,85 @@ public class LadderPlayer extends TimePlayer {
 		int numberOfBlackLadders = ladderLiberties.size();
 		ladderLiberties.addAll(libertiesOfLadders(WHITE));
 		IntSet twoLibertyLadders = twoLibertyGroups(BLACK);
-		int numberOfBlackTwoLibertyLadders = twoLibertyLadders.size();
 		twoLibertyLadders.addAll(twoLibertyGroups(WHITE));
-		boolean twoLiberties=false;
-		
+		boolean twoLiberties = false;
+
 		SearchNode root = getRoot();
 		// play out each ladder separately
 		Board ourCopy = new Board();
 		for (int i = 0; i < ladderLiberties.size() + twoLibertyLadders.size(); i++) {
-			
+
 			ourCopy.copyDataFrom(getBoard());
-			
+
 			int firstMove = 0;
-			int insidePlaysHere=0;
+			int insidePlaysHere = 0;
 			int insideColor;
-			if(i>=ladderLiberties.size()){
-				twoLiberties=true;
-				
-				IntSet twoLiberty = getBoard().getLiberties(twoLibertyLadders.get(i-ladderLiberties.size()));
-				int lib1= getBoard().getVacantNeighborCount(twoLiberty.get(0));
-				int lib2= getBoard().getVacantNeighborCount(twoLiberty.get(1));
-				if(lib1>lib2){
-					firstMove=twoLiberty.get(0);
-					if(ourCopy.isLegal(twoLiberty.get(0))){
+			if (i >= ladderLiberties.size()) {
+				twoLiberties = true;
+
+				IntSet twoLiberty = getBoard().getLiberties(
+						twoLibertyLadders.get(i - ladderLiberties.size()));
+				int lib1 = getBoard().getVacantNeighborCount(twoLiberty.get(0));
+				int lib2 = getBoard().getVacantNeighborCount(twoLiberty.get(1));
+				if (lib1 > lib2) {
+					firstMove = twoLiberty.get(0);
+					if (ourCopy.isLegal(twoLiberty.get(0))) {
 						ourCopy.play(twoLiberty.get(0));
 					}
-					insidePlaysHere=twoLiberty.get(1);
-				} else{
-					firstMove=twoLiberty.get(1);
-					if(ourCopy.isLegal(twoLiberty.get(1))){
+					insidePlaysHere = twoLiberty.get(1);
+				} else {
+					firstMove = twoLiberty.get(1);
+					if (ourCopy.isLegal(twoLiberty.get(1))) {
 						ourCopy.play(twoLiberty.get(1));
 					}
-					insidePlaysHere=twoLiberty.get(0);
+					insidePlaysHere = twoLiberty.get(0);
 				}
-				insideColor=ourCopy.getColor(twoLibertyLadders.get(i-ladderLiberties.size()));
-				
-			} else{
-				insidePlaysHere = ladderLiberties.get(i);
+				insideColor = ourCopy.getColor(twoLibertyLadders.get(i
+						- ladderLiberties.size()));
+
+			} else {
+				insidePlaysHere = getBoard().getLiberties(
+						ladderLiberties.get(i)).get(0);
 				insideColor = (i < numberOfBlackLadders) ? BLACK : WHITE;
 			}
 			// recordRunnable.copyDataFrom(getBoard());
-			
-			
+
 			int outsideColor = opposite(insideColor);
 			int winner;
 			int length = 0; // "length" of this ladder
 			boolean neighborAtari = false; // test to see if neighboring stones
 											// are in atari
-			
-			ourCopy.setColorToPlay(insideColor);
 
-			int stone = insidePlaysHere;
+			ourCopy.setColorToPlay(insideColor);
+			int stone;
+			int firstStone;
+			if(i>=ladderLiberties.size()){
+				stone = twoLibertyLadders.get(i-ladderLiberties.size());
+				firstStone= twoLibertyLadders.get(i-ladderLiberties.size());
+			}else{
+				stone = ladderLiberties.get(i);
+				firstStone= ladderLiberties.get(i);
+			}
 			do {
+				System.err.println(stone);
+				System.err.println(i);
 				for (int j = 0; j < 4; j++) {
 					int neighbor = getNeighbors(stone)[j];
-					if (ourCopy.getColor(neighbor) == ourCopy.getColor(stone)) {
+					if (ourCopy.getColor(neighbor) != ourCopy.getColor(stone)) {
 						if ((ourCopy.getColor(neighbor) == WHITE)
 								|| ourCopy.getColor(neighbor) == BLACK) {
-							if (ourCopy.isInAtari(neighbor)) {
+							if (ourCopy.isInAtari(ourCopy.getChainId(neighbor))) {
 								neighborAtari = true;
+								break;
 							}
 						}
 					}
 				}
+				if(neighborAtari){
+					break;
+				}
 				stone = ourCopy.getChainNextPoint(stone);
-				System.err.println(stone);
-			} while (stone != insidePlaysHere);
+			} while (stone != firstStone);
 			// runnable.getBoard().setColorToPlay(insideColor);
 			// recordRunnable.getBoard().setColorToPlay(insideColor);
 
@@ -170,134 +189,150 @@ public class LadderPlayer extends TimePlayer {
 			// player until
 			// the ladder is over (either the inside player is free or has been
 			// captured)
-			if(!neighborAtari){
-			while (true) {
+			if (!neighborAtari) {
+				while (true) {
 
-				// inside player policy: play in my only liberty
-				// Must check if move is legal first (not suicide)
+					// inside player policy: play in my only liberty
+					// Must check if move is legal first (not suicide)
 
-				// if(runnable.getBoard().isLegal(insidePlaysHere)){
-				// runnable.acceptMove(insidePlaysHere);
-				// }
-				if (ourCopy.isLegal(insidePlaysHere)) {
-					ourCopy.play(insidePlaysHere);
-				} else {
-					winner = outsideColor;
-					break;
-				}
-
-				if (ourCopy.getLibertyCount(insidePlaysHere) >= 3) {
-					// if (runnable.getBoard().getLibertyCount(insidePlaysHere)
-					// >= 3) {
-					// inside player is free
-					winner = insideColor;
-					break;
-				}
-
-				// outside player policy: play in the inside player's
-				// liberty with the most vacant neighbors
-				// IntSet insideLiberties =
-				// runnable.getBoard().getLiberties(insidePlaysHere);
-				IntSet insideLiberties = ourCopy.getLiberties(insidePlaysHere);
-				int mostVacantNeighbors = -1;
-				int pointToPlay = getFirstPointOnBoard();
-				for (int j = 0; j < insideLiberties.size(); j++) {
-					int lib = insideLiberties.get(j);
-					// int vacantNeighborCount =
-					// runnable.getBoard().getVacantNeighborCount(lib);
-					int vacantNeighborCount = ourCopy
-							.getVacantNeighborCount(lib);
-					if (vacantNeighborCount > mostVacantNeighbors) {
-						mostVacantNeighbors = vacantNeighborCount;
-						pointToPlay = lib;
+					// if(runnable.getBoard().isLegal(insidePlaysHere)){
+					// runnable.acceptMove(insidePlaysHere);
+					// }
+					if (ourCopy.isLegal(insidePlaysHere)) {
+						ourCopy.play(insidePlaysHere);
+					} else {
+						winner = outsideColor;
+						break;
 					}
-				}
-				// if(runnable.getBoard().isLegal(pointToPlay)){
-				// runnable.acceptMove(pointToPlay);
-				// }
-				if (ourCopy.isLegal(pointToPlay)) {
-					ourCopy.play(pointToPlay);
-				} else {
-					winner = insideColor;
-					break;
-				}
 
-				// if (runnable.getBoard().getColor(insidePlaysHere) !=
-				// insideColor) {
-				if (ourCopy.getColor(insidePlaysHere) != insideColor) {
-					// inside player was captured
-					winner = outsideColor;
-					break;
-				}
+					if (ourCopy.getLibertyCount(insidePlaysHere) >= 3) {
+						// if
+						// (runnable.getBoard().getLibertyCount(insidePlaysHere)
+						// >= 3) {
+						// inside player is free
+						winner = insideColor;
+						break;
+					}
 
-				// for loop looks through each of the neighbors for the newly
-				// placed inside stone
-				// checks each of these neighbors against the list of stones in
-				// atari and checks if they belong to the opposite color
-				// if an outside stone is in atari it sets winner to inside
-				// color and breaks.
-
-				for (int x = 0; x < 4; x++) {
-					int n = getNeighbors(insidePlaysHere)[x];
-					if (isOnBoard(n)) {
-
-						// if(runnable.getBoard().isInAtari(runnable.getBoard().getChainId(n))
-						// && runnable.getBoard().getColor(n)==outsideColor){
-						// neighborAtari=true;
-						// }
-						if (ourCopy.isInAtari(ourCopy.getChainId(n))
-								&& ourCopy.getColor(n) == outsideColor) {
-							neighborAtari = true;
+					// outside player policy: play in the inside player's
+					// liberty with the most vacant neighbors
+					// IntSet insideLiberties =
+					// runnable.getBoard().getLiberties(insidePlaysHere);
+					IntSet insideLiberties = ourCopy
+							.getLiberties(insidePlaysHere);
+					int mostVacantNeighbors = -1;
+					int pointToPlay = getFirstPointOnBoard();
+					for (int j = 0; j < insideLiberties.size(); j++) {
+						int lib = insideLiberties.get(j);
+						// int vacantNeighborCount =
+						// runnable.getBoard().getVacantNeighborCount(lib);
+						int vacantNeighborCount = ourCopy
+								.getVacantNeighborCount(lib);
+						if (vacantNeighborCount > mostVacantNeighbors) {
+							mostVacantNeighbors = vacantNeighborCount;
+							pointToPlay = lib;
 						}
 					}
-				}
-				if (neighborAtari) {
-					winner = insideColor;
-					break;
-				}
+					// if(runnable.getBoard().isLegal(pointToPlay)){
+					// runnable.acceptMove(pointToPlay);
+					// }
+					if (ourCopy.isLegal(pointToPlay)) {
+						ourCopy.play(pointToPlay);
+					} else {
+						winner = insideColor;
+						break;
+					}
 
-				// insidePlaysHere =
-				// runnable.getBoard().getLiberties(insidePlaysHere).get(0);
-				insidePlaysHere = ourCopy.getLiberties(insidePlaysHere).get(0);
-				length++;
+					// if (runnable.getBoard().getColor(insidePlaysHere) !=
+					// insideColor) {
+					if (ourCopy.getColor(insidePlaysHere) != insideColor) {
+						// inside player was captured
+						winner = outsideColor;
+						break;
+					}
 
-			}
-			}else{
-				winner=insideColor;
+					// for loop looks through each of the neighbors for the
+					// newly
+					// placed inside stone
+					// checks each of these neighbors against the list of stones
+					// in
+					// atari and checks if they belong to the opposite color
+					// if an outside stone is in atari it sets winner to inside
+					// color and breaks.
+
+					for (int x = 0; x < 4; x++) {
+						int n = getNeighbors(insidePlaysHere)[x];
+						if (isOnBoard(n)) {
+
+							// if(runnable.getBoard().isInAtari(runnable.getBoard().getChainId(n))
+							// &&
+							// runnable.getBoard().getColor(n)==outsideColor){
+							// neighborAtari=true;
+							// }
+							if (ourCopy.isInAtari(ourCopy.getChainId(n))
+									&& ourCopy.getColor(n) == outsideColor) {
+								neighborAtari = true;
+							}
+						}
+					}
+					if (neighborAtari) {
+						winner = insideColor;
+						break;
+					}
+
+					// insidePlaysHere =
+					// runnable.getBoard().getLiberties(insidePlaysHere).get(0);
+					insidePlaysHere = ourCopy.getLiberties(insidePlaysHere)
+							.get(0);
+					length++;
+
+				}
+			} else {
+				winner = insideColor;
 			}
 			// bias the search tree: call this playout for the winner.
 			// 100 is used as the number of runs incorporated, this needs to be
 			// tuned.
-			
-			if(twoLiberties){
+
+			if (twoLiberties) {
 				if (winner == insideColor) {
-					
+					System.err.println("INSIDE COLOR WON TWO LIBERTIES");
+					biased[firstMove] -= ladderMult ? length * ladderBias
+							: ladderBias;
+					root.addLosses(firstMove, ladderMult ? length * ladderBias
+							: ladderBias);
+
 				} else if (winner == outsideColor
 						&& outsideColor == getBoard().getColorToPlay()) {
-					System.err.println("bye");
-					biased[firstMove] += ladderMult ? length
-							* ladderBias : ladderBias;
-					root.addWins(firstMove, ladderMult ? length
-							* ladderBias : ladderBias);
+					biased[firstMove] += ladderMult ? length * ladderBias
+							: ladderBias;
+					root.addWins(firstMove, ladderMult ? length * ladderBias
+							: ladderBias);
 				}
-			} else{
-			if (winner == insideColor) {
-				biased[ladderLiberties.get(i)] += ladderMult ? length
-						* ladderBias : ladderBias;
-				root.addWins(ladderLiberties.get(i), ladderMult ? length
-						* ladderBias : ladderBias);
-			} else if (winner == outsideColor
-					&& insideColor == getBoard().getColorToPlay()) {
-				biased[ladderLiberties.get(i)] += ladderMult ? -length
-						* ladderBias : -ladderBias;
-				root.addLosses(ladderLiberties.get(i), ladderMult ? length
-						* ladderBias : ladderBias);
+			} else {
+				if (winner == insideColor) {
+					biased[getBoard().getLiberties(ladderLiberties.get(i)).get(
+							0)] += ladderMult ? length * ladderBias
+							: ladderBias;
+					root.addWins(getBoard()
+							.getLiberties(ladderLiberties.get(i)).get(0),
+							ladderMult ? length * ladderBias : ladderBias);
+				} else if (winner == outsideColor
+						&& insideColor == getBoard().getColorToPlay()) {
+					biased[getBoard().getLiberties(ladderLiberties.get(i)).get(
+							0)] += ladderMult ? -length * ladderBias
+							: -ladderBias;
+					root.addLosses(
+							getBoard().getLiberties(ladderLiberties.get(i))
+									.get(0), ladderMult ? length * ladderBias
+									: ladderBias);
+				}
+				// for (int j = 0; j < (ladderMult? length*ladderBias:
+				// ladderBias);
+				// j++) {
+				// incorporateRun(winner, recordRunnable);
+				// }
 			}
-			// for (int j = 0; j < (ladderMult? length*ladderBias: ladderBias);
-			// j++) {
-			// incorporateRun(winner, recordRunnable);
-			// }
-		}
 		}
 	}
 
