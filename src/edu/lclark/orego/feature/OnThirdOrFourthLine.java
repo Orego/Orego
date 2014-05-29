d5432dd1da267b3e07520d0fd998d8e667533ccc
Sam Levenick <slevenick@lclark.edu>
2014-05-29 09:56:18 -0700

    Added the feature OnThirdOrFourthLine and the test

---
+++ src/edu/lclark/orego/feature/OnThirdOrFourthLine.java
@@ -0,0 +1,21 @@
+package edu.lclark.orego.feature;
+
+import edu.lclark.orego.core.Board;
+
+public class OnThirdOrFourthLine implements Feature{
+	
+	public final Board board;
+	
+	public OnThirdOrFourthLine(Board board){
+		this.board=board;
+	}
+	
+	
+	@Override
+	public final boolean at(short p){
+		
+		
+		return false;
+	}
+
+}
