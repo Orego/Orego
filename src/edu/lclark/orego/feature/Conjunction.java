cdf3a6bbe5ef4f496c9cbc821f27be81ae5a77ee
Peter Drake <drake@lclark.edu>
2014-05-29 10:18:07 -0700

    Created part of test for Conjunction.

---
+++ src/edu/lclark/orego/feature/Conjunction.java
@@ -0,0 +1,25 @@
+package edu.lclark.orego.feature;
+
+public class Conjunction implements Feature {
+
+	private Feature a;
+	
+	private Feature b;
+	
+	public Conjunction(Feature a, Feature b) {
+		
+	}
+
+	@Override
+	public boolean at(short p) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/edu/lclark/orego/feature/ConjunctionTest.java
@@ -0,0 +1,54 @@
+package edu.lclark.orego.feature;
+
+import static edu.lclark.orego.core.Legality.OK;
+import static edu.lclark.orego.core.Legality.SUICIDE;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import edu.lclark.orego.core.Board;
+import edu.lclark.orego.core.CoordinateSystem;
+import static edu.lclark.orego.core.StoneColor.*;
+
+public class ConjunctionTest {
+
+	private Board board;
+	
+	private Conjunction conjunction;
+
+	private CoordinateSystem coords;
+	
+	/** Delegate method to call at on board. */
+	private short at(String label) {
+		return coords.at(label);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		board = new Board(7);
+		coords = board.getCoordinateSystem();
+//		conjunction = new Conjunction(new NotEyeLike(board), new OnThirdOrFourthLine(board));
+	}
+
+	@Test
+	public void testAt() {
+		String[] before = {
+				"......#",
+				".....#.",
+				"..#...#",
+				".#.#...",
+				"..#....",
+				".......",
+				".......",
+		};
+		board.setUpProblem(before, BLACK);
+		assertTrue(conjunction.at(at("c4")));
+		assertFalse(conjunction.at(at("g6")));
+		assertFalse(conjunction.at(at("d3")));
+	}
+
+	@Test
+	public void testClear() {
+		fail("Not yet implemented");
+	}
+
+}
