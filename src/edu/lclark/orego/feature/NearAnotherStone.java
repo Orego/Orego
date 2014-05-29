3e92b83d17376aa09578239dbf9ef92f51883135
Sam Levenick <slevenick@lclark.edu>
2014-05-29 11:55:14 -0700

    Changed the at for the NearAnotherStone

--- src/edu/lclark/orego/feature/NearAnotherStone.java
+++ src/edu/lclark/orego/feature/NearAnotherStone.java
@@ -1,18 +1,22 @@
 package edu.lclark.orego.feature;
 
 import edu.lclark.orego.core.Board;
+import edu.lclark.orego.util.BitBoard;
 
 public final class NearAnotherStone implements Feature{
 
 	private final Board board;
+	
+	private final BitBoard bitBoard;
 
 	public NearAnotherStone(Board board) {
 		this.board = board;
+		bitBoard = new BitBoard(board.getCoordinateSystem());
 	}
 	
 	@Override
 	public boolean at(short p){
-		return false;
+		return bitBoard.get(p);
 	}
 	
 	@Override
