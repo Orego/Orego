ee19efbca4be7b9af36e78ad67c0164c8f07acbd
Peter Drake <drake@lclark.edu>
2014-06-03 10:15:02 -0700

    Made Disjunction fields final.

--- src/edu/lclark/orego/feature/Disjunction.java
+++ src/edu/lclark/orego/feature/Disjunction.java
@@ -3,9 +3,9 @@ package edu.lclark.orego.feature;
 /** True if at least one of the features provided to the constructor is true. */
 public final class Disjunction implements Feature {
 
-	private Feature a;
+	private final Feature a;
 	
-	private Feature b;
+	private final Feature b;
 	
 	public Disjunction(Feature a, Feature b) {
 		this.a = a;
