514a66fa987029017921d413770066e915fad85a
Peter Drake <drake@lclark.edu>
2014-06-04 11:14:53 -0700

    Added scoring to RawPlayoutSpeed.

--- src/edu/lclark/orego/experiment/PlayoutComparison.java
+++ src/edu/lclark/orego/experiment/PlayoutComparison.java
@@ -2,18 +2,19 @@ package edu.lclark.orego.experiment;
 
 import ec.util.MersenneTwisterFast;
 import edu.lclark.orego.core.Board;
-import edu.lclark.orego.feature.*;
+import edu.lclark.orego.feature.Conjunction;
+import edu.lclark.orego.feature.Disjunction;
+import edu.lclark.orego.feature.Feature;
+import edu.lclark.orego.feature.NearAnotherStone;
+import edu.lclark.orego.feature.NotEyeLike;
+import edu.lclark.orego.feature.OnThirdOrFourthLine;
 import edu.lclark.orego.move.SimpleRandom;
-import edu.lclark.orego.score.*;
-import static edu.lclark.orego.core.StoneColor.*;
-import static edu.lclark.orego.core.NonStoneColor.*;
 
-public final class RawPlayoutSpeed {
-
+public class PlayoutComparison {
+	
 	public static void main(String[] args) {
 		MersenneTwisterFast random = new MersenneTwisterFast();
 		Board board = new Board(19);
-		Scorer scorer = new ChinesePlayoutScorer(board, 7.5);
 		Feature f = new Conjunction(
 				new NotEyeLike(board),
 				new Disjunction(
@@ -23,7 +24,6 @@ public final class RawPlayoutSpeed {
 //		SimpleRandom mover = new SimpleRandom(board, new NotEyeLike(board));
 		final int runs = 100000;
 		long total = 0;
-		int[] wins = new int[3];
 		for (int run = 0; run < runs; run++) {
 			long before = System.nanoTime();
 			board.clear();
@@ -32,15 +32,11 @@ public final class RawPlayoutSpeed {
 //				System.out.println(board.toString(p));
 //				System.out.println(board);
 			} while (board.getPasses() < 2);
-			wins[scorer.winner().index()]++;
 			long after = System.nanoTime();
 			total += (after - before);
 //			System.out.println((after - before) * 1.0 / board.getTurn());
 		}
 		System.out.println((runs / 1000.0) / (total / 1000000000.0) + " kpps");
-		System.out.println("Black wins: " + wins[BLACK.index()]);
-		System.out.println("White wins: " + wins[WHITE.index()]);
-		System.out.println("Ties: " + wins[VACANT.index()]);
 	}
 
 }
