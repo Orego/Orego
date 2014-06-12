51ec250083c36604189cf70b6181185613fb84ab
Sam Levenick <slevenick@lclark.edu>
2014-06-12 15:17:20 -0700

    Added a logger to the PlayoutSpeed that records the time to finish each playout and the number of moves in a plain text file.

--- src/edu/lclark/orego/experiment/PlayoutSpeed.java
+++ src/edu/lclark/orego/experiment/PlayoutSpeed.java
@@ -1,5 +1,14 @@
 package edu.lclark.orego.experiment;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 import ec.util.MersenneTwisterFast;
 import edu.lclark.orego.core.Board;
 import edu.lclark.orego.feature.StoneCounter;
@@ -11,9 +20,20 @@ import static edu.lclark.orego.core.NonStoneColor.*;
 
 /** Tests the speed of playouts in one thread. */
 public final class PlayoutSpeed {
+	
+	private final static Logger logger = Logger.getLogger(PlayoutSpeed.class.getName()) ;
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
+		FileHandler handler;
+		try {
+			handler = new FileHandler("%h/orego" + new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime()));
+			handler.setFormatter(new PlainTextFormatter());
+			logger.addHandler(handler);
+		} catch (IOException e) {
+			logger.log(Level.INFO, e.toString());
+		}
+		logger.setLevel(Level.ALL);
 		MersenneTwisterFast random = new MersenneTwisterFast();
 		Board original = new Board(19);
 		Board copy = new Board(19);
@@ -29,13 +49,16 @@ public final class PlayoutSpeed {
 		int playoutLength = 0;
 		for (int run = 0; run < runs; run++) {
 			long before = System.nanoTime();
+			int turns = 0;
 			copy.copyDataFrom(original);
 			do {
 				mover.selectAndPlayOneMove(random);
-				playoutLength++;
+				turns++;
 			} while (copy.getPasses() < 2 && mercyObserver.mercyWinner()==null);
 			wins[scorer.winner().index()]++;
+			playoutLength += turns;
 			long after = System.nanoTime();
+			logger.log(Level.FINE, ((after - before) / 1000000.0) + ", " + turns + "\n"); 
 			total += (after - before);
 		}
 		System.out.println((runs / 1000.0) / (total / 1000000000.0) + " kpps");
