3ad1853409f8230504dfaca63509457353c04fd9
Maggie Dreyer <mdreyer@lclark.edu>
2014-06-30 14:49:51 -0700

    Added the RaveDescender and some tests

---
+++ src/edu/lclark/orego/mcts/RaveDescender.java
@@ -0,0 +1,57 @@
+package edu.lclark.orego.mcts;
+
+import static java.lang.Float.NEGATIVE_INFINITY;
+import static edu.lclark.orego.core.CoordinateSystem.*;
+import edu.lclark.orego.mcts.RaveNode;
+import edu.lclark.orego.core.Board;
+
+public final class RaveDescender extends BestRateDescender{
+	
+	/**
+	 * This corresponds to b^2/(0.5*0.5) in Silver's formula. The higher this
+	 * is, the less attention is paid to RAVE.
+	 */
+	private float raveBias;
+
+	public RaveDescender(Board board, TranspositionTable table, int biasDelay) {
+		super(board, table, biasDelay);
+		raveBias = 0.0009f;
+	}
+	
+	/**
+	 * Uses the formula from here:
+	 * David Silver 
+	 * Reinforcement Learning and Simulation-Based Search in Computer Go 
+	 * A thesis submitted to the Faculty of Graduate Studies and Research 
+	 *	in partial ful���llment of the requirements for the degree of 
+	 *	Doctor of Philosophy 
+	 *	Department of Computing Science 
+	 * http://papersdb.cs.ualberta.ca/~papersdb/uploaded_files/1029/paper_thesis.pdf
+	 * equation 8.40 page 107//Updated
+	 */
+	@Override
+	float searchValue(SearchNode node, short move){
+		if (node.getWins(move) == Integer.MIN_VALUE) {
+			return NEGATIVE_INFINITY;
+		}
+		if (move == PASS) {
+			return node.getWinRate(move);
+		}
+		RaveNode raveNode = (RaveNode) node;
+		float c = raveNode.getRuns(move);
+		float r = raveNode.getWinRate(move);
+		float rc = raveNode.getRaveRuns(move);
+		float rr = raveNode.getRaveWinRate(move);
+		float coef = raveCoefficient(c, rc);
+		return r * (1 - coef) + rr * coef;
+	}
+	
+	/**
+	 * Returns the weight given to RAVE (as opposed to direct MC data) given c
+	 * runs and rc RAVE runs.
+	 */
+	protected float raveCoefficient(float c, float rc) {
+		return rc / (rc + c + rc * c * raveBias);
+	}
+
+}
---
+++ src/edu/lclark/orego/mcts/RaveDescenderTest.java
@@ -0,0 +1,55 @@
+package edu.lclark.orego.mcts;
+
+import static org.junit.Assert.*;
+import static edu.lclark.orego.core.StoneColor.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.lclark.orego.core.CoordinateSystem;
+
+public class RaveDescenderTest {
+	
+	Player player;
+	McRunnable runnable;
+	TranspositionTable table;
+	RaveDescender descender;
+	RaveTreeUpdater updater;
+
+	@Before
+	public void setUp() throws Exception {
+		player = new Player(1, CopiableStructureFactory.feasible(5));
+		runnable = player.getMcRunnable(0);
+		CoordinateSystem coords = player.getBoard().getCoordinateSystem();
+		table = new TranspositionTable(100, new RaveNodeBuilder(coords), coords);
+		descender = new RaveDescender(player.getBoard(), table, 0);
+		updater = new RaveTreeUpdater(player.getBoard(), table, 0);
+		player.setTreeDescender(descender);
+		player.setTreeUpdater(updater);
+	}
+	
+	/** Delegate method to call at on coords. */
+	private short at(String label) {
+		return player.getBoard().getCoordinateSystem().at(label);
+	}
+
+	@Test
+	public void testIncorporateRun() {
+		player.acceptMove(at("a1"));
+		RaveNode root = (RaveNode) updater.getRoot();
+		runnable.copyDataFrom(player.getBoard());
+		runnable.acceptMove(at("a2"));
+		runnable.acceptMove(at("a3"));
+		runnable.acceptMove(at("a4"));
+		updater.updateTree(BLACK, runnable);
+		assertEquals(0.333333, root.getWinRate(at("a2")), 0.01);
+		runnable.copyDataFrom(player.getBoard());
+		runnable.acceptMove(at("a4"));
+		runnable.acceptMove(at("a3"));
+		runnable.acceptMove(at("a2"));
+		updater.updateTree(BLACK, runnable);
+		assertEquals(0.25, root.getRaveWinRate(at("a2")), 0.01);
+		assertEquals(0.2856f, descender.searchValue(root, at("a2")), 0.01f);
+	}
+
+}
