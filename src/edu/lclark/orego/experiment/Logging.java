aaa3e55e6dde218af83d076fe6c5a49c2a073ef0
Maggie Dreyer <mdreyer@lclark.edu>
2014-07-17 09:41:54 -0700

    working on logging

--- src/edu/lclark/orego/experiment/Logging.java
+++ src/edu/lclark/orego/experiment/Logging.java
@@ -1,14 +1,31 @@
 package edu.lclark.orego.experiment;
 
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Logging {
-	
+
 	private static String logFilePath;
-	
-	public static void setFilePath(String filePath){
+
+	private static Logger logger = Logger.getLogger("orego-default");
+
+	public static void setFilePath(String filePath) {
 		logFilePath = filePath;
+		try {
+			FileHandler handler = new FileHandler(filePath);
+			handler.setFormatter(new PlainTextFormatter());
+			logger.addHandler(handler);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
-	
-	public static String getFilePath(){
+
+	public static String getFilePath() {
 		return logFilePath;
 	}
 
