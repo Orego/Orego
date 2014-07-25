77c41deaf122b63271c870db4f3f1b27ffacda4a
Sam Levenick <slevenick@lclark.edu>
2014-07-25 15:25:52 -0700

    Added gogui analyze commands

--- src/edu/lclark/orego/mcts/Player.java
+++ src/edu/lclark/orego/mcts/Player.java
@@ -568,5 +568,71 @@ public final class Player {
 				}
 				return result;
 	}
+	
+	@SuppressWarnings("boxing")
+	public String goguiGetWinrate(){
+		// TODO Encapsulate this normalization in a single place, called by all
+				// the various gogui methods
+				float min = Float.MAX_VALUE;
+				float max = 0.0f;
+				for (short p : coords.getAllPointsOnBoard()) {
+					if (board.getColorAt(p) == VACANT) {
+						float winRate = getRoot().getWinRate(p);
+						if(winRate>0){
+							min = Math.min(min, winRate);
+							max = Math.max(max, winRate);
+					}
+					}
+				}
+				String result = "";
+				for (short p : coords.getAllPointsOnBoard()) {
+					if (getBoard().getColorAt(p) == VACANT) {
+						float winRate= getRoot().getWinRate(p);
+						if (winRate > 0) {
+							if (result.length() > 0) {
+								result += "\n";
+							}
+							int green = (int) (255*(winRate - min) / (max - min));
+							result += String.format("COLOR %s %s\nLABEL %s %.0f%%",
+									String.format("#%02x%02x00",  255-green, green),
+									coords.toString(p), coords.toString(p), (winRate * 100));
+						}
+					}
+				}
+				return result;
+	}
+	
+	@SuppressWarnings("boxing")
+	public String goguiGetRuns(){
+		// TODO Encapsulate this normalization in a single place, called by all
+				// the various gogui methods
+				float min = Float.MAX_VALUE;
+				float max = 0.0f;
+				for (short p : coords.getAllPointsOnBoard()) {
+					if (board.getColorAt(p) == VACANT) {
+						float wins = getRoot().getRuns(p);
+						if(wins>0){
+							min = Math.min(min, wins);
+							max = Math.max(max, wins);
+					}
+					}
+				}
+				String result = "";
+				for (short p : coords.getAllPointsOnBoard()) {
+					if (getBoard().getColorAt(p) == VACANT) {
+						float wins= getRoot().getRuns(p);
+						if (wins > 0) {
+							if (result.length() > 0) {
+								result += "\n";
+							}
+							int green = (int) (255*(wins - min) / (max - min));
+							result += String.format("COLOR %s %s\nLABEL %s %.0f",
+									String.format("#%02x%02x00",  255-green, green),
+									coords.toString(p), coords.toString(p), wins);
+						}
+					}
+				}
+				return result;
+	}
 
 }
