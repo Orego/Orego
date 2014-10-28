f8417d81215194109dc33d24687f2e82179c9a17
Peter Drake <drake@lclark.edu>
2014-10-28 14:33:16 -0700

    Added a test to verify that tiger's mouths are rated low (if the last move was not nearby).

--- src/edu/lclark/orego/patterns/ShapeTableTest.java
+++ src/edu/lclark/orego/patterns/ShapeTableTest.java
@@ -1,9 +1,13 @@
 package edu.lclark.orego.patterns;
 
+import static edu.lclark.orego.experiment.PropertyPaths.OREGO_ROOT;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import edu.lclark.orego.core.Board;
+import edu.lclark.orego.core.CoordinateSystem;
 
 public class ShapeTableTest {
 
@@ -15,7 +19,7 @@ public class ShapeTableTest {
 	}
 
 	@Test
-	public void test() {
+	public void testOverlap() {
 		float f = table.getScalingFactor();
 		table.update(1, true);
 		assertEquals(f * 0.5 + 1 * (1 - f), table.getWinRate(1), 0.0001f);
@@ -27,5 +31,28 @@ public class ShapeTableTest {
 		float b = f * a + 1 * (1 - f);
 		assertEquals((a + (b*2)) / 3, table.getWinRate(70000), 0.0001f);
 	}
+	
+	@Test
+	public void testTigersMouth() {
+		// Playing into a Tiger's mouth should be a terrible idea
+		table = new ShapeTable(OREGO_ROOT
+				+ "patterns/patterns3stones-SHAPE-sf999.data");
+		Board board = new Board(9);
+		CoordinateSystem coords = board.getCoordinateSystem();
+		board.play("d5");
+		board.pass();
+		board.play("f5");
+		board.pass();
+		board.play("e6");
+		// TODO Why does getHash need that last argument?
+		// TODO Should getHash really be a static method?
+		long hash = PatternFinder.getHash(board, coords.at("e5"), 3, coords.at("a1"));
+		// Note that this test would fail if the last move (a1 above) were changed
+		// to one of the nearby stones, because such a move is so rare that the table
+		// slot would consist entirely of noise.
+		assertTrue(table.getWinRate(hash) < 0.25);
+	}
+	
+
 
 }
