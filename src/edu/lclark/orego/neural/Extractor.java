1f2f9314a16553eb77dd8b7f13cbe290a8bc62d3
Peter Drake <drake@lclark.edu>
2015-06-04 09:55:45 -0700

    Added method to check for black stones

--- src/edu/lclark/orego/neural/Extractor.java
+++ src/edu/lclark/orego/neural/Extractor.java
@@ -1,5 +1,34 @@
 package edu.lclark.orego.neural;
 
+import edu.lclark.orego.core.Board;
+import edu.lclark.orego.core.CoordinateSystem;
+import static edu.lclark.orego.core.StoneColor.*;
+
+/** Extracts board information for processing by neural network. */
 public class Extractor {
+
+	private Board board;
+
+	public Extractor(Board board) {
+		this.board = board;
+	}
+
+	/**
+	 * Returns 1 if there is a black stone at specified coordinates, otherwise
+	 * 0.
+	 */
+	public double isBlack(int row, int col) {
+		CoordinateSystem coords = board.getCoordinateSystem();
+		if (!coords.isValidOneDimensionalCoordinate(row)) {
+			return 0;
+		}
+		if (!coords.isValidOneDimensionalCoordinate(col)) {
+			return 0;
+		}
+		if (board.getColorAt(coords.at(row, col)) == BLACK) {
+			return 1;
+		}
+		return 0;
+	}
 
 }
