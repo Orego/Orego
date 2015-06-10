734b461be80ef80690a28950add2ddba3d6bc7ab
gdaaronson <gdaaronson@gmail.com>
2015-06-10 15:43:34 -0700

    In the process of taking input from KGS games, still needs work

---
+++ src/edu/lclark/orego/neural/KGSExperiment.java
@@ -0,0 +1,106 @@
+package edu.lclark.orego.neural;
+
+import static edu.lclark.orego.core.CoordinateSystem.PASS;
+import static edu.lclark.orego.core.NonStoneColor.VACANT;
+import static edu.lclark.orego.move.Mover.PRIMES;
+
+import java.io.File;
+import java.util.List;
+
+import edu.lclark.orego.patterns.PatternExtractor.*;
+import edu.lclark.orego.core.Board;
+import edu.lclark.orego.core.CoordinateSystem;
+import edu.lclark.orego.sgf.SgfParser;
+import edu.lclark.orego.thirdparty.MersenneTwisterFast;
+import edu.lclark.orego.util.ShortSet;
+
+public class KGSExperiment {
+
+	
+	private SgfParser parser;
+
+	private Board board;
+
+	private CoordinateSystem coords;
+	
+	private final MersenneTwisterFast random;
+
+	public static void main(String[] args) {
+		new KGSExperiment().run();
+	}
+
+	private void run() {
+		final List<List<Short>> games = parser.parseGamesFromFile(new File(
+				"sgf-test-files/19/1977-02-27.sgf"), 179);
+		int i = 0;
+
+		Network net = new Network(19 * 19 * 4, 19 * 19, 1, 19 * 19);
+		double[][] training = new double[179][19 * 19 * 4];
+		double[][] trainingCorrect = new double[179][4];
+		
+		for (final List<Short> game : games) {
+			int k = 0;
+			for (final Short move : game) {
+				board.play(move);
+				int p = 0; // place in training array
+				Extractor extractor = new Extractor(board);
+				for (int row = 0; row < 19; row++) {
+					for (int col = 0; col < 19; col++) {
+						training[k][p] = extractor.isBlack(row, col);
+						p++;
+					}
+				}
+				for (int row = 0; row < 19; row++) {
+					for (int col = 0; col < 19; col++) {
+						training[k][p] = extractor.isWhite(row, col);
+						p++;
+					}
+				}
+				for (int row = 0; row < 19; row++) {
+					for (int col = 0; col < 19; col++) {
+						training[k][p] = extractor.isUltimateMove(row, col);
+						p++;
+					}
+				}
+				for (int row = 0; row < 19; row++) {
+					for (int col = 0; col < 19; col++) {
+						training[k][p] = extractor.isPenultimateMove(row, col);
+						p++;
+					}
+				}
+				trainingCorrect[k] = new double[] {move, selectRandomMove(move)};
+				System.out.println(trainingCorrect[k][0]);
+				net.train(1, (int) trainingCorrect[k][0], training[k]);
+				net.train(0, (int) trainingCorrect[k][1], training[k]);
+				k++;
+			}
+		}
+	}
+	
+	public KGSExperiment() {
+		board = new Board(19);
+		coords = board.getCoordinateSystem();
+		parser = new SgfParser(coords, true);
+		random = new MersenneTwisterFast();
+	}
+	
+	short selectRandomMove(short move) {
+		ShortSet vacantPoints = board.getVacantPoints();
+		short start = (short) (random.nextInt(vacantPoints.size()));
+		short i = start;
+		short skip = PRIMES[random.nextInt(PRIMES.length)];
+		do {
+			short p = vacantPoints.get(i);
+			if ((board.getColorAt(p) == VACANT)) {
+				if (board.isLegal(p) && p != move) {
+					return p;
+				}
+			}
+			// Advancing by a random prime skips through the array
+			// in a manner analogous to double hashing.
+			i = (short) ((i + skip) % vacantPoints.size());
+		} while (i != start);
+		return PASS;
+	}
+
+}
