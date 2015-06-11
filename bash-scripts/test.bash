58c2c03d03bec16eea0829b138d0e07189fd729a
Peter Drake <drake@lclark.edu>
2015-06-11 14:57:43 -0700

    Still flailing about with test.bash.

--- bash-scripts/test.bash
+++ bash-scripts/test.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 gcloud compute instances create broadcast --disk name=orego-instance,boot=yes
-gcloud compute ssh broadcast ./update
+gcloud compute ssh --ssh-flag="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" broadcast ./update
 gcloud compute instances delete broadcast --quiet
