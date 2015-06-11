90393fab711c6385194d8c12a705f36534336bc0
Peter Drake <drake@lclark.edu>
2015-06-11 10:20:21 -0700

    Extended cloudcast.bash to do git updating.

--- bash-scripts/cloudcast.bash
+++ bash-scripts/cloudcast.bash
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-# NOTE: Before running this script, create an instance using the disk orego-instance, pull the latest Orego code
-# from GitHub, and build it.
+# Get the root persistent disk orego-instance up-to-date with the latest code from GitHub
+gcloud compute instances create broadcast --disk name=orego-instance,boot=yes
+gcloud compute ssh --ssh-flag="-A" broadcast <<REMOTE
+cd Orego
+git pull origin cloud
+./build-all.sh
+REMOTE
+gcloud compute instances delete broadcast
 
+# Create an image from that disk
 gcloud compute images delete exp1-image
 gcloud compute images create exp1-image --source-disk orego-instance
+
+# Create the instances
 gcloud compute instances create broadcast --disk name=orego-instance,boot=yes
 gcloud compute instances create instance1 --image exp1-image --machine-type n1-highcpu-2
 gcloud compute instances create instance2 --image exp1-image --machine-type n1-highcpu-2
+
+# Tell the broadcast instance to launch the experiment across the other instances
 eval `ssh-agent`
 ssh-add ~/.ssh/google_compute_engine
 gcloud compute ssh --ssh-flag="-A" broadcast <<REMOTE
