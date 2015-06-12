c3f97328b1bce40efc697204652f308156037fb5
drake <drake@lclark.edu>
2015-06-12 08:29:53 -0700

    Fixed script problems by adding sleep 30s.

--- bash-scripts/cloudcast.bash
+++ bash-scripts/cloudcast.bash
@@ -4,9 +4,9 @@
 
 # Get the root persistent disk orego-instance up-to-date with the latest code from GitHub
 gcloud compute instances create broadcast --disk name=orego-instance,boot=yes
-eval `ssh-agent`
-ssh-add ~/.ssh/google_compute_engine
-gcloud compute ssh --ssh-flag="-AT" broadcast ./update
+# Wait for ssh demon to start on instance
+sleep 30s
+gcloud compute ssh --ssh-flag="-A" broadcast ./update.bash
 gcloud compute instances delete broadcast --quiet
 
 # Create an image from that disk
@@ -17,10 +17,9 @@ gcloud compute images create exp1-image --source-disk orego-instance
 gcloud compute instances create broadcast --disk name=orego-instance,boot=yes
 gcloud compute instances create instance1 --image exp1-image --machine-type n1-highcpu-2
 gcloud compute instances create instance2 --image exp1-image --machine-type n1-highcpu-2
+sleep 30s
 
 # Tell the broadcast instance to launch the experiment across the other instances
-eval `ssh-agent`
-ssh-add ~/.ssh/google_compute_engine
-gcloud compute ssh --ssh-flag="-AT" broadcast <<REMOTE
+gcloud compute ssh --ssh-flag="-A" broadcast <<REMOTE
 ./broadcast.bash
 REMOTE
--- bash-scripts/test.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gcloud compute instances create broadcast --disk name=orego-instance,boot=yes
-gcloud compute ssh --ssh-flag="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" broadcast ./update
-gcloud compute instances delete broadcast --quiet
