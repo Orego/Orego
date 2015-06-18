94636464e049e57cbe7f064f5e87c44341999481
drake <drake@lclark.edu>
2015-06-17 20:22:13 -0700

    Can I get output if I send it do stderr?

--- src/edu/lclark/orego/experiment/PlayerListener.java
+++ src/edu/lclark/orego/experiment/PlayerListener.java
@@ -30,7 +30,7 @@ final class PlayerListener implements Runnable {
 				// s is passed in in case there is a multi-line error message,
 				// so game can dump all of the message to the output file
 				final String line = s.nextLine();
-				log("PlayerListener got line " + line);
+				System.err.println("PlayerListener got line " + line);
 				if (!line.isEmpty()) {
 					finishedNormally = game.handleResponse(line, s);
 				}
