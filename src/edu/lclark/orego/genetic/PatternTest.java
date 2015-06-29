ea745dd08aa21ec7daa54cf9c94db2c403c41979
andreadean5 <andreadean@lclark.edu>
2015-06-29 13:05:12 -0700

    added test

---
+++ src/edu/lclark/orego/genetic/PatternTest.java
@@ -0,0 +1,28 @@
+package edu.lclark.orego.genetic;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import edu.lclark.orego.core.Board;
+
+public class PatternTest {
+
+	@Test
+	public void testPatternMatches() {
+		Pattern pattern = new Pattern();
+		Board board = new Board(5);
+		board.play("a5");
+		board.play("d2");
+		// System.out.println(board.toString());
+		int friendly = 1;
+		int enemy = 0b1000000000000000000;
+		int vacant = 0b1111110111110111111111110;
+		assertTrue(pattern.patternMatches((short) 21, board, friendly,
+				enemy, vacant));
+		assertFalse(pattern.patternMatches((short) 21, board, enemy,
+				enemy, vacant));
+	}
+
+}
