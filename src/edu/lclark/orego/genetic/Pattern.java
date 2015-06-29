1c67f7af85bc54d6f0b8c42d5ea3085583a47e99
andreadean5 <andreadean@lclark.edu>
2015-06-29 13:00:04 -0700

    Can match pattern.

--- src/edu/lclark/orego/genetic/Pattern.java
+++ src/edu/lclark/orego/genetic/Pattern.java
@@ -2,6 +2,7 @@ package edu.lclark.orego.genetic;
 
 import edu.lclark.orego.core.Board;
 import edu.lclark.orego.core.Color;
+import edu.lclark.orego.core.NonStoneColor;
 import edu.lclark.orego.core.CoordinateSystem;
 import edu.lclark.orego.feature.HistoryObserver;
 
@@ -22,23 +23,35 @@ public class Pattern {
 	
 
 	
-	boolean patternMatches(short p, Board board, int ... pattern){
+	public boolean patternMatches(short p, Board board, int ... pattern){
 		CoordinateSystem coords = board.getCoordinateSystem();
-		boolean matches = true;
+		int actualFriendly = 0;
+		int actualEnemy = 0;
+		int actualVacant = 0;
 		int row = coords.row(p);
 		int col = coords.column(p);
-		for (int r = row -2; r < row + 2; r++){
-			for (int c = col - 2; c < col + 2; c++){
+		int i = 0;
+		for (int r = row -2; r <= row + 2; r++){
+			for (int c = col - 2; c <= col + 2; c++){
 				if ((c != col) || (r != row)){
 					short temp = coords.at(r, c);
 					final Color color = board.getColorAt(temp);
 					if (color == board.getColorToPlay()) {
-						// Friendly stone at this neighbor
+						actualFriendly |= (1 << i);
+					} else if (color == board.getColorToPlay().opposite()){
+						actualEnemy |= (1 << i);
+					} else if (color == NonStoneColor.VACANT){
+						actualVacant |= (1 << i);
 					}
 				}
+				i++;
 			}
 		}
-		return matches;
+		
+		
+		System.out.println(Integer.toBinaryString(actualFriendly) + " " + Integer.toBinaryString(actualEnemy) + " " + Integer.toBinaryString(actualVacant));
+		return ((((actualFriendly  << 6) & (pattern[0] << 6)) == (actualFriendly << 6)) 
+				&& ((actualEnemy & pattern[1]) == actualEnemy) 
+				&& ((actualVacant & pattern[2]) == actualVacant));
 	}
-
 }
