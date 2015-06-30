84b66f640a51b90747bbed9efb73806216dbbfb0
andreadean5 <andreadean@lclark.edu>
2015-06-30 14:40:17 -0700

    Split up methods more

--- src/edu/lclark/orego/genetic/Pattern.java
+++ src/edu/lclark/orego/genetic/Pattern.java
@@ -14,6 +14,37 @@ public class Pattern {
 	 */
 
 	/**
+	 * Given a row or column i and the width of the board, returns "actual",
+	 * which will be actualFriendly or actualEnemy in the space patternMatcher
+	 */
+	private int edgePattern(int i, int width) {
+		int actual = 0;
+		if (i == 0) {
+			actual |= (4 << 24);
+		} else if (i == 1) {
+			actual |= (2 << 24);
+		} else if (i == 2) {
+			actual |= (6 << 24);
+		} else if (i == width - 3) {
+			actual |= (1 << 24);
+		} else if (i == width - 2) {
+			actual |= (3 << 24);
+		} else if (i == width - 1) {
+			actual |= (5 << 24);
+		}
+		return actual;
+	}
+
+	public boolean patternMatcher(short p, Board board,
+			HistoryObserver historyObserver, int... pattern) {
+		if (pattern[0] > 0) {
+			return spaceMatcher(p, board, pattern);
+		} else {
+			return timeMatcher(p, board, historyObserver, pattern);
+		}
+	}
+
+	/**
 	 * Given a position on the board (short) that is assumed to be empty, a
 	 * board, and three ints containing the pattern, this returns a
 	 * boolean--true if the pattern matches the condition of the position. Bit
@@ -21,68 +52,41 @@ public class Pattern {
 	 * in actualFriendly are equal to the row number. For left and right edges,
 	 * 3 bits (24-27) in actualEnemy are equal to the column number.
 	 */
-	public boolean patternMatcher(short p, Board board, HistoryObserver historyObserver, int... pattern) {
+	private boolean spaceMatcher(short p, Board board, int... pattern) {
 		CoordinateSystem coords = board.getCoordinateSystem();
-		int actualFriendly = 0;
-		int actualEnemy = 0;
 		int actualVacant = 0;
-		if (pattern[0] > 0) {
-			int row = coords.row(p);
-			int col = coords.column(p);
-			if (row == 0) {
-				actualFriendly |= (4 << 24);
-			} else if (row == 1) {
-				actualFriendly |= (2 << 24);
-			} else if (row == 2) {
-				actualFriendly |= (6 << 24);
-			} else if (row == coords.getWidth() - 3) {
-				actualFriendly |= (1 << 24);
-			} else if (row == coords.getWidth() - 2) {
-				actualFriendly |= (3 << 24);
-			} else if (row == coords.getWidth() - 1) {
-				actualFriendly |= (5 << 24);
-			}
-			if (col == 0) {
-				actualEnemy |= (4 << 24);
-			} else if (col == 1) {
-				actualEnemy |= (2 << 24);
-			} else if (col == 2) {
-				actualEnemy |= (6 << 24);
-			} else if (col == coords.getWidth() - 3) {
-				actualEnemy |= (1 << 24);
-			} else if (col == coords.getWidth() - 2) {
-				actualEnemy |= (3 << 24);
-			} else if (col == coords.getWidth() - 1) {
-				actualEnemy |= (5 << 24);
-			}
-
-//			actualFriendly |= (row << 24);
-//			actualEnemy |= (col << 24);
-			int i = 0;
-			for (int r = row - 2; r <= row + 2; r++) {
-				for (int c = col - 2; c <= col + 2; c++) {
-					if ((c != col) || (r != row)) {
-						short temp = coords.at(r, c);
-						final Color color = board.getColorAt(temp);
-						if (color == board.getColorToPlay()) {
-							actualFriendly |= (1 << i);
-						} else if (color == board.getColorToPlay().opposite()) {
-							actualEnemy |= (1 << i);
-						} else if (color == NonStoneColor.VACANT) {
-							actualVacant |= (1 << i);
-						}
+		int row = coords.row(p);
+		int col = coords.column(p);
+		int actualFriendly = edgePattern(row, coords.getWidth());
+		int actualEnemy = edgePattern(col, coords.getWidth());
+		int i = 0;
+		for (int r = row - 2; r <= row + 2; r++) {
+			for (int c = col - 2; c <= col + 2; c++) {
+				if ((c != col) || (r != row)) {
+					short temp = coords.at(r, c);
+					final Color color = board.getColorAt(temp);
+					if (color == board.getColorToPlay()) {
+						actualFriendly |= (1 << i);
+					} else if (color == board.getColorToPlay().opposite()) {
+						actualEnemy |= (1 << i);
+					} else if (color == NonStoneColor.VACANT) {
+						actualVacant |= (1 << i);
 					}
-					i++;
 				}
+				i++;
 			}
-		} else {
-			 
-			
 		}
 		// System.out.println(Integer.toBinaryString(actualFriendly) + " "
 		// + Integer.toBinaryString(actualEnemy) + " "
 		// + Integer.toBinaryString(actualVacant));
 		return ((((actualFriendly) & (pattern[0])) == (actualFriendly))
 				&& (((actualEnemy) & (pattern[1])) == (actualEnemy)) && ((actualVacant & pattern[2]) == actualVacant));
+
+	}
+
+	private boolean timeMatcher(short p, Board board,
+			HistoryObserver historyObserver, int[] pattern) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
