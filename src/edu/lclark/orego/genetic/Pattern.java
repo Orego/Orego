f09d9f683d9969bad550983c9cb8b3fa8f2ffd40
andreadean5 <andreadean@lclark.edu>
2015-06-30 09:52:37 -0700

    added javadoc

--- src/edu/lclark/orego/genetic/Pattern.java
+++ src/edu/lclark/orego/genetic/Pattern.java
@@ -7,21 +7,21 @@ import edu.lclark.orego.core.CoordinateSystem;
 import edu.lclark.orego.feature.HistoryObserver;
 
 public class Pattern {
-	
+
 	/**
-	 *  friendly = 			100		4
-	 *  enemy = 			010  	2
-	 *  vacant = 			001 	1
-	 *  
-	 *  
-	 *  friendly, enemy = 	110 	6
-	 *  all three 			111 	7
-	 *  enemy or vacant = 	011   	3
-	 *  friendly or v	= 	101		5
-	 *  off-board			000		0
+	 * friendly = 100 4 enemy = 010 2 vacant = 001 1
+	 * 
+	 * 
+	 * friendly, enemy = 110 6 all three 111 7 enemy or vacant = 011 3 friendly
+	 * or v = 101 5 off-board 000 0
 	 */
-	
-	public boolean patternMatches(short p, Board board, int ... pattern){
+
+	/**
+	 * Given a position on the board (short) that is assumed to be empty, a
+	 * board, and three ints containing the pattern, this returns a
+	 * boolean--true if the pattern matches the condition of the position.
+	 */
+	public boolean patternMatcher(short p, Board board, int... pattern) {
 		CoordinateSystem coords = board.getCoordinateSystem();
 		int actualFriendly = 0;
 		int actualEnemy = 0;
@@ -29,27 +29,23 @@ public class Pattern {
 		int row = coords.row(p);
 		int col = coords.column(p);
 		int i = 0;
-		for (int r = row -2; r <= row + 2; r++){
-			for (int c = col - 2; c <= col + 2; c++){
-				if ((c != col) || (r != row)){
+		for (int r = row - 2; r <= row + 2; r++) {
+			for (int c = col - 2; c <= col + 2; c++) {
+				if ((c != col) || (r != row)) {
 					short temp = coords.at(r, c);
 					final Color color = board.getColorAt(temp);
 					if (color == board.getColorToPlay()) {
 						actualFriendly |= (1 << i);
-					} else if (color == board.getColorToPlay().opposite()){
+					} else if (color == board.getColorToPlay().opposite()) {
 						actualEnemy |= (1 << i);
-					} else if (color == NonStoneColor.VACANT){
+					} else if (color == NonStoneColor.VACANT) {
 						actualVacant |= (1 << i);
 					}
 				}
 				i++;
 			}
 		}
-		// System.out.println(Integer.toBinaryString(actualFriendly) + " " +
-		// Integer.toBinaryString(actualEnemy) + " " +
-		// Integer.toBinaryString(actualVacant));
-		return ((((actualFriendly  << 6) & (pattern[0] << 6)) == (actualFriendly << 6)) 
-				&& ((actualEnemy & pattern[1]) == actualEnemy) 
-				&& ((actualVacant & pattern[2]) == actualVacant));
+		return ((((actualFriendly << 6) & (pattern[0] << 6)) == (actualFriendly << 6))
+				&& ((actualEnemy & pattern[1]) == actualEnemy) && ((actualVacant & pattern[2]) == actualVacant));
 	}
 }
