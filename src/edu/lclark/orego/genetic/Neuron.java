5156be9957b810dd4d8291dac1f9e991db9122f6
Peter Drake <drake@lclark.edu>
2015-07-04 17:00:35 -0700

    Features are now extracted once across the entire board.

--- src/edu/lclark/orego/genetic/Neuron.java
+++ src/edu/lclark/orego/genetic/Neuron.java
@@ -12,11 +12,35 @@ public class Neuron {
 
 	private long inhibition;
 
+	/**
+	 * Extracts features for each point on the board.
+	 * 
+	 * @param features
+	 *            Results are written into this array, which has one element per
+	 *            board (or board-adjacent) point.
+	 */
+	public static void extractFeatures(Board board, long[] features) {
+		final CoordinateSystem coords = board.getCoordinateSystem();
+		for (short p = 0; p < features.length; p++) {
+			long temp = board.getColorAt(p).index();
+			// Invert stone colors if it's white's turn
+			if (board.getColorToPlay() == WHITE && temp < 2) {
+				temp = 1 - temp;
+			}
+			features[p] = 1L << temp;
+		}
+		
+	}
+
+	// TODO Separate extracting features per point and gathering them per
+	// neighborhood
 	// TODO Does this belong here or in ConvolutionalLayer?
 	/**
 	 * Extracts the features around point p into the long array features.
 	 * 
-	 * @param feature An array of length 8, with one element for each neighbor of p. Results are written into this array. 
+	 * @param feature
+	 *            An array of length 8, with one element for each neighbor of p.
+	 *            Results are written into this array.
 	 */
 	public static void extractFeaturesAround(short p, Board board,
 			long[] features) {
@@ -54,9 +78,11 @@ public class Neuron {
 		System.out.println(Integer.toBinaryString(-2));
 	}
 
-	public boolean isActive(long... inputs) {
+	/** Returns true if this neuron is active at point p. */
+	public boolean isActiveAt(short p, long[] featureMap, CoordinateSystem coords) {
 		int sum = 0;
-		for (long input : inputs) {
+		for (short n : coords.getNeighbors(p)) {
+			long input = featureMap[n];
 			sum += countBits(excitation & input);
 			sum -= countBits(inhibition & input);
 		}
--- src/edu/lclark/orego/genetic/NeuronTest.java
+++ src/edu/lclark/orego/genetic/NeuronTest.java
@@ -3,17 +3,29 @@ package edu.lclark.orego.genetic;
 import static org.junit.Assert.*;
 import static edu.lclark.orego.genetic.Neuron.*;
 import static edu.lclark.orego.core.StoneColor.*;
+import static edu.lclark.orego.core.CoordinateSystem.*;
 import org.junit.Before;
 import org.junit.Test;
 
 import edu.lclark.orego.core.Board;
+import edu.lclark.orego.core.CoordinateSystem;
 
 public class NeuronTest {
 
 	private Neuron neuron;
 	
+	private Board board;
+	
+	private CoordinateSystem coords;
+	
+	private short at(String label) {
+		return coords.at(label);
+	}
+
 	@Before
 	public void setUp() throws Exception {
+		board = new Board(5);
+		coords = board.getCoordinateSystem();
 	}
 
 	@Test
@@ -27,13 +39,51 @@ public class NeuronTest {
 
 	@Test
 	public void testIsActive() {
-		// For this neuron to fire, there must be 3 more bits in position 0 (rightmost) than
-		// in position 1
+		// For this neuron to fire, there must be 3 more friendly stones than enemy stones in the receptive field
 		neuron = new Neuron(3, 0b01L, 0b10L);
-		assertTrue(neuron.isActive(0b01L, 0b11L, 0b01L, 0b01L));
-		assertTrue(neuron.isActive(0b01L, 0b01L, 0b01L, 0b01L));
-		assertFalse(neuron.isActive(0b11L, 0b11L, 0b01L, 0b01L));
-		assertFalse(neuron.isActive(0b01L, 0b11L, 0b01L, 0b00L));
+		String[] diagram = {
+				"..O..",
+				"#O.O.",
+				".#O..",
+				"##...",
+				".#...",
+		};
+		board.setUpProblem(diagram, BLACK);
+		long[] features = new long[coords.getFirstPointBeyondExtendedBoard()];
+		extractFeatures(board, features);
+		assertTrue(neuron.isActiveAt(at("a1"), features, coords));
+		assertTrue(neuron.isActiveAt(at("a3"), features, coords));
+		assertFalse(neuron.isActiveAt(at("c2"), features, coords));
+		board.setUpProblem(diagram, WHITE);
+		extractFeatures(board, features);
+		assertTrue(neuron.isActiveAt(at("c4"), features, coords));
+		assertFalse(neuron.isActiveAt(at("d3"), features, coords));
+	}
+	
+	@Test
+	public void testExtractFeatures() {
+		String[] diagram = {
+				".....",
+				".....",
+				".....",
+				"#....",
+				".O...",
+		};
+		board.setUpProblem(diagram, BLACK);
+		//n w e s nw ne sw se
+		//ovef
+		long[] features = new long[coords.getFirstPointBeyondExtendedBoard()];
+		extractFeatures(board, features);
+		assertEquals(0b0001, features[at("a2")]); // Friendly
+		assertEquals(0b0010, features[at("b1")]); // Enemy
+		assertEquals(0b0100, features[at("a1")]); // Vacant
+		assertEquals(0b1000, features[coords.getNeighbors(at("a1"))[WEST_NEIGHBOR]]); // Off board
+		board.setColorToPlay(WHITE);
+		extractFeatures(board, features);
+		assertEquals(0b0010, features[at("a2")]); // Enemy
+		assertEquals(0b0001, features[at("b1")]); // Friendly
+		assertEquals(0b0100, features[at("a1")]); // Vacant
+		assertEquals(0b1000, features[coords.getNeighbors(at("a1"))[WEST_NEIGHBOR]]); // Off board
 	}
 	
 	@Test
@@ -45,17 +95,16 @@ public class NeuronTest {
 				"#....",
 				".O...",
 		};
-		Board board = new Board(5);
 		board.setUpProblem(diagram, BLACK);
 		//n w e s nw ne sw se
 		//ovef
 		long[] extracted = new long[8];
 		long[] blackToPlay = {0b0001, 0b1000, 0b0010, 0b1000, 0b1000, 0b0100, 0b1000, 0b1000};
-		extractFeaturesAround(board.getCoordinateSystem().at("a1"), board, extracted);
+		extractFeaturesAround(at("a1"), board, extracted);
 		assertArrayEquals(blackToPlay, extracted);
 		board.setColorToPlay(WHITE);
 		long[] whiteToPlay = {0b0010, 0b1000, 0b0001, 0b1000, 0b1000, 0b0100, 0b1000, 0b1000};
-		extractFeaturesAround(board.getCoordinateSystem().at("a1"), board, extracted);
+		extractFeaturesAround(at("a1"), board, extracted);
 		assertArrayEquals(whiteToPlay, extracted);
 	}
 
