f2523fb28569dcc236d10f613e1ef3cca9fd33e6
Peter Drake <drake@lclark.edu>
2015-07-06 09:36:47 -0700

    Remove getBitCount, because Java has one built in.

--- src/edu/lclark/orego/genetic/Neuron.java
+++ src/edu/lclark/orego/genetic/Neuron.java
@@ -1,6 +1,7 @@
 package edu.lclark.orego.genetic;
 
 import edu.lclark.orego.core.Board;
+import static java.lang.Long.*;
 import edu.lclark.orego.core.CoordinateSystem;
 import static edu.lclark.orego.core.StoneColor.*;
 
@@ -20,7 +21,6 @@ public class Neuron {
 	 *            board (or board-adjacent) point.
 	 */
 	public static void extractFeatures(Board board, long[] features) {
-		final CoordinateSystem coords = board.getCoordinateSystem();
 		for (short p = 0; p < features.length; p++) {
 			long temp = board.getColorAt(p).index();
 			// Invert stone colors if it's white's turn
@@ -32,45 +32,10 @@ public class Neuron {
 		
 	}
 
-	// TODO Separate extracting features per point and gathering them per
-	// neighborhood
-	// TODO Does this belong here or in ConvolutionalLayer?
-	/**
-	 * Extracts the features around point p into the long array features.
-	 * 
-	 * @param feature
-	 *            An array of length 8, with one element for each neighbor of p.
-	 *            Results are written into this array.
-	 */
-	public static void extractFeaturesAround(short p, Board board,
-			long[] features) {
-		final CoordinateSystem coords = board.getCoordinateSystem();
-		final short[] neighbors = coords.getNeighbors(p);
-		for (int i = 0; i < features.length; i++) {
-			long temp = board.getColorAt(neighbors[i]).index();
-			// Invert stone colors if it's white's turn
-			if (board.getColorToPlay() == WHITE && temp < 2) {
-				temp = 1 - temp;
-			}
-			features[i] = 1L << temp;
-		}
-	}
-
 	public Neuron(int threshold, long excitation, long inhibition) {
 		this.threshold = threshold;
 		this.excitation = excitation;
 		this.inhibition = inhibition;
-	}
-
-	/** Returns the number of bits set in bits. */
-	static int countBits(long bits) {
-		// Brian Kernighan's method, adapted from:
-		// https://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetKernighan
-		int count;
-		for (count = 0; bits != 0; count++) {
-			bits &= bits - 1; // clear the least significant bit set
-		}
-		return count;
 	}
 
 	public static void main(String[] args) {
@@ -83,8 +48,8 @@ public class Neuron {
 		int sum = 0;
 		for (short n : coords.getNeighbors(p)) {
 			long input = featureMap[n];
-			sum += countBits(excitation & input);
-			sum -= countBits(inhibition & input);
+			sum += bitCount(excitation & input);
+			sum -= bitCount(inhibition & input);
 		}
 		return sum >= threshold;
 	}
--- src/edu/lclark/orego/genetic/NeuronTest.java
+++ src/edu/lclark/orego/genetic/NeuronTest.java
@@ -29,15 +29,6 @@ public class NeuronTest {
 	}
 
 	@Test
-	public void testCountBits() {
-		assertEquals(0, countBits(0L));
-		assertEquals(3, countBits(0b00101001L));
-		assertEquals(1, countBits(Long.MIN_VALUE));
-		assertEquals(64, countBits(-1));
-		assertEquals(36, countBits(0b0100110001101111011011100110011101001010011011110110100001101110L));
-	}
-
-	@Test
 	public void testIsActive() {
 		// For this neuron to fire, there must be 3 more friendly stones than enemy stones in the receptive field
 		neuron = new Neuron(3, 0b01L, 0b10L);
@@ -84,28 +75,6 @@ public class NeuronTest {
 		assertEquals(0b0001, features[at("b1")]); // Friendly
 		assertEquals(0b0100, features[at("a1")]); // Vacant
 		assertEquals(0b1000, features[coords.getNeighbors(at("a1"))[WEST_NEIGHBOR]]); // Off board
-	}
-	
-	@Test
-	public void testExtractFeaturesAround() {
-		String[] diagram = {
-				".....",
-				".....",
-				".....",
-				"#....",
-				".O...",
-		};
-		board.setUpProblem(diagram, BLACK);
-		//n w e s nw ne sw se
-		//ovef
-		long[] extracted = new long[8];
-		long[] blackToPlay = {0b0001, 0b1000, 0b0010, 0b1000, 0b1000, 0b0100, 0b1000, 0b1000};
-		extractFeaturesAround(at("a1"), board, extracted);
-		assertArrayEquals(blackToPlay, extracted);
-		board.setColorToPlay(WHITE);
-		long[] whiteToPlay = {0b0010, 0b1000, 0b0001, 0b1000, 0b1000, 0b0100, 0b1000, 0b1000};
-		extractFeaturesAround(at("a1"), board, extracted);
-		assertArrayEquals(whiteToPlay, extracted);
 	}
 
 }
