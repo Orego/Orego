768cffb00941c3d0559a9715bb2efdac699f0f64
Peter Drake <drake@lclark.edu>
2015-07-07 14:53:24 -0700

    Implemented Genotype.cross.

--- src/edu/lclark/orego/genetic/Genotype.java
+++ src/edu/lclark/orego/genetic/Genotype.java
@@ -4,33 +4,57 @@ import edu.lclark.orego.thirdparty.MersenneTwisterFast;
 
 public class Genotype {
 	
-	private long[] genotype;
+	private long[] bits;
 	
-	public Genotype(long [] genotype){
-		this.genotype = genotype;
+	public Genotype(long[] bits){
+		this.bits = bits;
 	}
 	
-	public Genotype(int numberOfGenotypes){
-		genotype = new long[numberOfGenotypes];
+	/**
+	 * @param length The number of 64-bit longs in this genotype.
+	 */
+	public Genotype(int length){
+		bits = new long[length];
 	}
 	
-	public void setRules(long [] genotype){
-		this.genotype = genotype;
+	public void setBits(long[] bits){
+		this.bits = bits;
 	}
 	
-	public void generateRules(){
+	public void randomize(){
 		MersenneTwisterFast random = new MersenneTwisterFast();
-		for(long i: genotype){
+		for (int i = 0; i < bits.length; i++) {
+			bits[i] = random.nextLong();
 		}
 	}
 	
-	public Genotype performCrossover(Genotype A, Genotype B){
-		return null;
+	public Genotype cross(Genotype that, MersenneTwisterFast random){
+		int k = random.nextInt(64*bits.length + 1);
+		int word = k / 64;
+		int i = k % 64;
+		long[] result = new long[bits.length];
+		for (int j = 0; j < result.length; j++) {
+			if(j < word){
+				result[j] = bits[j];
+			}else if (j == word){
+				if(i == 0){
+					result[j] = that.bits[j];
+				}else{
+					result[j] = (bits[j] & (((1L << i) - 1L) << (64 - i))) | (that.bits[j] & ((1L << (64 - i)) - 1L));
+				}
+				
+			}else{
+				result[j] = that.bits[j];
+			}
+		}
+		return new Genotype(result);
 	}
 	
-	public long[] getGenotype(){
-		return genotype;
+	public long[] getBits(){
+		return bits;
 	}
 	
-	
+	public static void main(String[] args) {
+		System.out.println(Long.toBinaryString(-1L));
+	}
 }
--- src/edu/lclark/orego/genetic/GenotypeTest.java
+++ src/edu/lclark/orego/genetic/GenotypeTest.java
@@ -4,11 +4,39 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import edu.lclark.orego.thirdparty.MersenneTwisterFast;
+
 public class GenotypeTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCross() {
+		long bit0[] = {0L, 0L};
+		long bit1[] = {-1L, -1L};
+		Genotype a = new Genotype(bit0);
+		Genotype b = new Genotype(bit1);
+		int count0 = 0;
+		int count64 = 0;
+		int count127 = 0;
+		int trials = 1000000;
+		MersenneTwisterFast random = new MersenneTwisterFast();
+		for(int i = 0; i < trials; i++){
+			Genotype c = a.cross(b, random);
+			if(((c.getBits()[0] >>> 63) & 1L) == 1){
+				count0++;
+			}
+			if(((c.getBits()[1] >>> 63) & 1L) == 1){
+				count64++;
+			}
+			if((c.getBits()[1] & 1L ) == 1){
+				count127++;
+			}
+		}
+		assertEquals(1.0/129, 1.0*count0/trials, .05);
+		assertEquals(65.0/129, 1.0*count64/trials, .05);
+		assertEquals(128.0/129, 1.0*count127/trials, .05);
+		assertTrue(1.0*count0/trials > 0);
+		assertTrue(1.0*count127/trials < 1);
 	}
+
 
 }
