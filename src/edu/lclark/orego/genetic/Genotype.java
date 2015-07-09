c7247071b04f04a51c4bd7024acd01e2c8ecd2eb
Peter Drake <drake@lclark.edu>
2015-07-09 09:22:48 -0700

    Wrote some method signatures in Genotype for fitness evaluation.

--- src/edu/lclark/orego/genetic/Genotype.java
+++ src/edu/lclark/orego/genetic/Genotype.java
@@ -3,64 +3,81 @@ package edu.lclark.orego.genetic;
 import edu.lclark.orego.thirdparty.MersenneTwisterFast;
 
 public class Genotype {
-	
-	private long[] words;
-	
-	public Genotype(long[] words){
-		this.words = words;
+
+	public static void main(String[] args) {
+		System.out.println(Long.toBinaryString(-1L));
 	}
-	
+
+	private double fitness;
+
+	private long[] words;
+
 	/**
-	 * @param length The number of 64-bit longs in this genotype.
+	 * @param length
+	 *            The number of 64-bit longs in this genotype.
 	 */
-	public Genotype(int length){
+	public Genotype(int length) {
 		words = new long[length];
 	}
-	
-	public void setWords(long[] words){
+
+	public Genotype(long[] words) {
 		this.words = words;
 	}
-	
-	public void mutate(MersenneTwisterFast random){
-		int r = random.nextInt(64 * words.length);
-		int w = r / 64;
-		words[w] ^= (1L << r); 
-	}
-	
-	public void randomize(){
-		MersenneTwisterFast random = new MersenneTwisterFast();
-		for (int i = 0; i < words.length; i++) {
-			words[i] = random.nextLong();
-		}
-	}
-	
-	public Genotype cross(Genotype that, MersenneTwisterFast random){
-		int k = random.nextInt(64*words.length + 1);
+
+	public Genotype cross(Genotype that, MersenneTwisterFast random) {
+		int k = random.nextInt(64 * words.length + 1);
 		int w = k / 64;
 		int i = k % 64;
 		long[] result = new long[words.length];
 		for (int j = 0; j < result.length; j++) {
-			if(j < w){
+			if (j < w) {
 				result[j] = words[j];
-			}else if (j == w){
-				if(i == 0){
+			} else if (j == w) {
+				if (i == 0) {
 					result[j] = that.words[j];
-				}else{
-					result[j] = (words[j] & (((1L << i) - 1L) << (64 - i))) | (that.words[j] & ((1L << (64 - i)) - 1L));
+				} else {
+					result[j] = (words[j] & (((1L << i) - 1L) << (64 - i)))
+							| (that.words[j] & ((1L << (64 - i)) - 1L));
 				}
-				
-			}else{
+
+			} else {
 				result[j] = that.words[j];
 			}
 		}
 		return new Genotype(result);
 	}
-	
-	public long[] getWords(){
+
+	public void evaluateFitness() {
+
+	}
+
+	public double getFitness() {
+		return fitness;
+	}
+
+	public long[] getWords() {
 		return words;
 	}
-	
-	public static void main(String[] args) {
-		System.out.println(Long.toBinaryString(-1L));
+
+	/** Returns the number of moves that phenotype correctly predicts from game. */
+	int hits(short[] game, Phenotype phenotype) {
+		return -1;
+	}
+
+	public void mutate(MersenneTwisterFast random) {
+		int r = random.nextInt(64 * words.length);
+		int w = r / 64;
+		words[w] ^= (1L << r);
+	}
+
+	public void randomize() {
+		MersenneTwisterFast random = new MersenneTwisterFast();
+		for (int i = 0; i < words.length; i++) {
+			words[i] = random.nextLong();
+		}
+	}
+
+	public void setWords(long[] words) {
+		this.words = words;
 	}
 }
