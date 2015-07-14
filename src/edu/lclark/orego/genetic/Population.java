f9de8b8f6460f21d89af8e3f1ec481eb57db32bd
Peter Drake <drake@lclark.edu>
2015-07-14 10:56:02 -0700

    Changed Population to allow experiment.

--- src/edu/lclark/orego/genetic/Population.java
+++ src/edu/lclark/orego/genetic/Population.java
@@ -18,10 +18,12 @@ public class Population {
 
 	public static final int NUMBER_OF_THREADS = 32;
 	
+	public static final int NUMBER_OF_CONTEXTS = 1000;
+	
 	public static final int NUMBER_OF_REPLIES = 1000;
 
 	public static void main(String[] args) {
-		new Population(500).evolve(50);
+		new Population(1000).evolve(50);
 	}
 
 	private Genotype[] individuals;
@@ -32,7 +34,7 @@ public class Population {
 		random = new MersenneTwisterFast();
 		individuals = new Genotype[individualCount];
 		for (int i = 0; i < individualCount; i++) {
-			individuals[i] = new Genotype((NUMBER_OF_REPLIES / 2) + 64 * 19 + 361 * 361 * 8 + 361);
+			individuals[i] = new Genotype(NUMBER_OF_REPLIES / 2, NUMBER_OF_CONTEXTS);
 			individuals[i].randomize();
 		}
 	}
