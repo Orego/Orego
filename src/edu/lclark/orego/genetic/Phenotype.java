a2f2a339317b42c8dfe739db350adcb639a75afa
andreadean5 <andreadean@lclark.edu>
2015-07-14 10:36:22 -0700

    formatting

--- src/edu/lclark/orego/genetic/Phenotype.java
+++ src/edu/lclark/orego/genetic/Phenotype.java
@@ -28,31 +28,28 @@ import java.util.*;
 
 @SuppressWarnings("serial")
 public class Phenotype implements Mover {
-	
+
 	private LgrfTable table;
 
 	private Set<Long> contexts;
-	
+
 	private Board board;
-	
+
 	private CoordinateSystem coords;
-	
+
 	private static final short[][][] NEIGHBORHOODS = new short[MAX_POSSIBLE_BOARD_WIDTH + 1][][];
-	
-	public static final short[][] OFFSETS = { 
-		{ 0, -1 }, { 0, 1 }, { -1, 0 },{ 1, 0 }, 
-		{ -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 }, 
-		{ -2, 0 }, { 2, 0 }, { 0, -2 }, { 0, 2 }, 
-		{ -2, -1 }, { -2, 1 }, { -1, -2 },{ -1, 2 }, 
-		{ 2, 1 }, { 2, -1 }, { 1, -2 }, { 1, 2 }, 
-		{ 2, 2 },{ 2, -2 }, { -2, 2 }, { -2, -2 },
-		{ 3, 0 }, { -3, 0 }, { 0, -3 }, { 0, 3 }, 
-		{ 4, 0 }, { -4, 0 }, { 0, -4 }, { 0, 4 }};
-	
-	public short[] getNeighborhood(short p){
+
+	public static final short[][] OFFSETS = { { 0, -1 }, { 0, 1 }, { -1, 0 },
+			{ 1, 0 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 }, { -2, 0 },
+			{ 2, 0 }, { 0, -2 }, { 0, 2 }, { -2, -1 }, { -2, 1 }, { -1, -2 },
+			{ -1, 2 }, { 2, 1 }, { 2, -1 }, { 1, -2 }, { 1, 2 }, { 2, 2 },
+			{ 2, -2 }, { -2, 2 }, { -2, -2 }, { 3, 0 }, { -3, 0 }, { 0, -3 },
+			{ 0, 3 }, { 4, 0 }, { -4, 0 }, { 0, -4 }, { 0, 4 } };
+
+	public short[] getNeighborhood(short p) {
 		return NEIGHBORHOODS[coords.getWidth()][p];
 	}
-	
+
 	private short[] findNeighborhood(short p) {
 		final int r = coords.row(p), c = coords.column(p);
 		final short[] result = new short[OFFSETS.length];
@@ -72,7 +69,7 @@ public class Phenotype implements Mover {
 		// Create a small array and copy the elements into it
 		return java.util.Arrays.copyOf(result, count);
 	}
-	
+
 	/**
 	 * Large-knight neighborhoods around points. First index is point around
 	 * which neighborhood is defined.
@@ -84,7 +81,8 @@ public class Phenotype implements Mover {
 		synchronized (NEIGHBORHOODS) {
 			if (NEIGHBORHOODS[width] == null) {
 				final short[] pointsOnBoard = coords.getAllPointsOnBoard();
-				NEIGHBORHOODS[width] = new short[coords.getFirstPointBeyondBoard()][];
+				NEIGHBORHOODS[width] = new short[coords
+						.getFirstPointBeyondBoard()][];
 				for (final short p : pointsOnBoard) {
 					NEIGHBORHOODS[width][p] = findNeighborhood(p);
 				}
@@ -93,29 +91,30 @@ public class Phenotype implements Mover {
 		}
 	}
 
-	public long contextAt(short p){
+	public long contextAt(short p) {
 		short[] neighborhood = getNeighborhood(p);
 		long context = 0;
 		int shift = 0;
-		for(int i = 0; i < neighborhood.length; i++, shift += 2){
+		for (int i = 0; i < neighborhood.length; i++, shift += 2) {
 			long c = board.getColorAt(neighborhood[i]).index();
-			if (c < 2 && board.getColorToPlay() == WHITE) { // If it's a stone color
+			if (c < 2 && board.getColorToPlay() == WHITE) { // If it's a stone
+															// color
 				c = 1 - c;
 			}
 			context |= c << shift;
 		}
 		return context;
 	}
-	
+
 	/** MASKS[i] has the i lowest-order bits on. */
 	public static final long[] MASKS = new long[64];
-	
+
 	static {
 		for (int i = 0; i < MASKS.length; i++) {
 			MASKS[i] = (1L << i) - 1;
 		}
 	}
-	
+
 	/**
 	 * @param replyLongs
 	 *            Number of longs in each genotype specifying replies (rather
@@ -143,11 +142,11 @@ public class Phenotype implements Mover {
 			contexts.add(words[i]);
 		}
 	}
-	
+
 	private HistoryObserver history;
-	
+
 	private Predicate filter;
-	
+
 	public Phenotype(Board board) {
 		this.board = board;
 		coords = board.getCoordinateSystem();
@@ -166,20 +165,23 @@ public class Phenotype implements Mover {
 		final short penultimate = history.get(board.getTurn() - 2);
 		short reply = table.getSecondLevelReply(board.getColorToPlay(),
 				penultimate, ultimate);
-		if (coords.isOnBoard(reply) && (board.getColorAt(reply) == VACANT) && filter.at(reply) && board.isLegal(reply)) {
+		if (coords.isOnBoard(reply) && (board.getColorAt(reply) == VACANT)
+				&& filter.at(reply) && board.isLegal(reply)) {
 			return reply;
 		}
 		reply = table.getFirstLevelReply(board.getColorToPlay(), ultimate);
-		if (coords.isOnBoard(reply) && (board.getColorAt(reply) == VACANT) && filter.at(reply) && board.isLegal(reply)) {
+		if (coords.isOnBoard(reply) && (board.getColorAt(reply) == VACANT)
+				&& filter.at(reply) && board.isLegal(reply)) {
 			return reply;
 		}
-		// TODO Check for contexts
-		final short start = (short) random.nextInt(board.getVacantPoints().size());
+		final short start = (short) random.nextInt(board.getVacantPoints()
+				.size());
 		short i = start;
 		final short skip = PRIMES[random.nextInt(PRIMES.length)];
 		do {
 			final short p = board.getVacantPoints().get(i);
-			if (board.isLegal(p) && filter.at(p) && containsContext(contextAt(p))){
+			if (board.isLegal(p) && filter.at(p)
+					&& containsContext(contextAt(p))) {
 				return p;
 			}
 			// Advancing by a random prime skips through the array
@@ -188,7 +190,7 @@ public class Phenotype implements Mover {
 		} while (i != start);
 		return NO_POINT;
 	}
-	
+
 	@Override
 	public short selectAndPlayOneMove(MersenneTwisterFast random, boolean fast) {
 		short p = bestMove(random);
@@ -196,26 +198,34 @@ public class Phenotype implements Mover {
 		return p;
 	}
 
-	short getReply(StoneColor colorToPlay, short penultimateMove, short previousMove) {
-		short p = table.getSecondLevelReply(colorToPlay, penultimateMove, previousMove);
+	short getReply(StoneColor colorToPlay, short penultimateMove,
+			short previousMove) {
+		short p = table.getSecondLevelReply(colorToPlay, penultimateMove,
+				previousMove);
 		if (p == NO_POINT) {
 			return table.getFirstLevelReply(colorToPlay, previousMove);
 		}
 		return p;
 	}
-	
-	public void setReply(StoneColor colorToPlay, short penultimateMove, short previousMove, short reply) {
+
+	public void setReply(StoneColor colorToPlay, short penultimateMove,
+			short previousMove, short reply) {
 		if (coords.isOnBoard(reply)) {
 			if (previousMove == NO_POINT || coords.isOnBoard(previousMove)) {
-				if (!(penultimateMove == NO_POINT || coords.isOnBoard(penultimateMove))) {
+				if (!(penultimateMove == NO_POINT || coords
+						.isOnBoard(penultimateMove))) {
 					penultimateMove = RESIGN;
 				}
-				table.setReply(colorToPlay, penultimateMove, previousMove, reply);				
+				table.setReply(colorToPlay, penultimateMove, previousMove,
+						reply);
 			}
 		}
 	}
 
-	/** Returns the number of moves that this phenotype correctly predicts from game. */
+	/**
+	 * Returns the number of moves that this phenotype correctly predicts from
+	 * game.
+	 */
 	@SuppressWarnings("boxing")
 	public int hits(Short[] game) {
 		MersenneTwisterFast random = new MersenneTwisterFast();
@@ -229,31 +239,28 @@ public class Phenotype implements Mover {
 		}
 		return result;
 	}
-	
+
 	/** Note that # indicates a friendly stone, O an enemy stone. */
-	public String contextToString(long a){
+	public String contextToString(long a) {
 		char[] c = new char[32];
-		for (int i = 0; i < c.length; i++){
+		for (int i = 0; i < c.length; i++) {
 			long temp = (a & (0b11L << (i * 2))) >>> (i * 2);
-			if (temp == 0){
+			if (temp == 0) {
 				c[i] = BLACK.toChar();
-			} else if (temp == 1){
+			} else if (temp == 1) {
 				c[i] = WHITE.toChar();
-			} else if (temp == 2){
+			} else if (temp == 2) {
 				c[i] = VACANT.toChar();
 			} else {
 				c[i] = OFF_BOARD.toChar();
 			}
 		}
-		String result = 
-				"    " + c[29] + "\n"
-				+ "    " + c[25] + "\n"
-				+ "  " + c[23] + c[12] + c[8] + c[13] + c[22] + "\n"
-				+ "  " + c[14] + c[4] + c[2] + c[5] + c[15] + "\n"
-				+ c[30] + c[26] + c[10] + c[0] + " " + c[1] + c[11] + c[27] + c[31] + "\n" 
-				+ "  " + c[18] + c[6] + c[3] + c[7] + c[19] + "\n"
-				+ "  " + c[21] + c[17] + c[9] + c[16] + c[20] + "\n"
-				+ "    " + c[24] + "\n"
+		String result = "    " + c[29] + "\n" + "    " + c[25] + "\n" + "  "
+				+ c[23] + c[12] + c[8] + c[13] + c[22] + "\n" + "  " + c[14]
+				+ c[4] + c[2] + c[5] + c[15] + "\n" + c[30] + c[26] + c[10]
+				+ c[0] + " " + c[1] + c[11] + c[27] + c[31] + "\n" + "  "
+				+ c[18] + c[6] + c[3] + c[7] + c[19] + "\n" + "  " + c[21]
+				+ c[17] + c[9] + c[16] + c[20] + "\n" + "    " + c[24] + "\n"
 				+ "    " + c[28] + "\n";
 		return result;
 	}
